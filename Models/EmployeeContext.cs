6c89d46f59bddb2da18da6f949943a2b777e8a50
Frank Hangler <frank@plotandscatter.com>
2019-11-27 11:35:08 -0800

    Initial app version

---
+++ Models/EmployeeContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeApi.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
