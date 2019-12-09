c5da61947db46363d600e4782d5506867747873b
Frank Hangler <frank@plotandscatter.com>
2019-12-09 13:29:54 -0800

    Change exiting employees link

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -34,7 +34,7 @@ export class NavMenu extends Component {
                   <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+                  <NavLink tag={Link} className="text-dark" to="/employees">Exiting employees</NavLink>
                 </NavItem>
               </ul>
             </Collapse>
