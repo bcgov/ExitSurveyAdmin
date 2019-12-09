18c4b5d3699f04ef0304e45a19a6e0a568fe7b1e
Frank Hangler <frank@plotandscatter.com>
2019-12-09 11:45:16 -0800

    Break up name, add BirthDate

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,8 +1,12 @@
+using System;
+
 namespace EmployeeApi.Models
 {
     public class Employee
     {
         public long Id { get; set; }
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
     }
 }
