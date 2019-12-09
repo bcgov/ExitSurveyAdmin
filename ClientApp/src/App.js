6736f3f9ac4e38f44a6b23e338a31e04d4bfa5f2
Frank Hangler <frank@plotandscatter.com>
2019-12-09 14:37:19 -0800

    Refactor

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { AdminUserListing } from './components/AdminUsers/AdminUserListing'
+import { EmployeeListing } from './components/Employees/EmployeeListing';
+import { AdminUserListing } from './components/AdminUsers/AdminUserListing';
 
 import './custom.css'
 
@@ -14,7 +14,7 @@ export default class App extends Component {
     return (
       <Layout>
         <Route exact path='/' component={Home} />
-        <Route path='/employees' component={FetchData} />
+        <Route path='/employees' component={EmployeeListing} />
         <Route path='/admin-users' component={AdminUserListing} />
       </Layout>
     );
