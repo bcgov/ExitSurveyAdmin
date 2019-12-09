7eb4925b2fdc373348b30f4eba39a50457506893
Frank Hangler <frank@plotandscatter.com>
2019-12-09 12:29:35 -0800

    Seed the database

---
+++ Migrations/20191209201953_InitialCreate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ExitSurveyAdmin.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    FirstName = table.Column<string>(maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(maxLength: 50, nullable: false),
+                    BirthDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,12 +1,24 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ExitSurveyAdmin.Models
 {
-    public class Employee
-    {
-        public long Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public DateTime BirthDate { get; set; }
-    }
+  public class Employee
+  {
+    [Required]
+    public string Id { get; set; }
+
+    [StringLength(50, MinimumLength = 1)]
+    [Required]
+    public string FirstName { get; set; }
+
+    [StringLength(50, MinimumLength = 1)]
+    [Required]
+    public string LastName { get; set; }
+
+
+    [DataType(DataType.Date)]
+    [Required]
+    public DateTime BirthDate { get; set; }
+  }
 }
