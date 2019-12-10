e98015af6be5c9a534f37f647a28e8ab4b3e9ffd
Frank Hangler <frank@plotandscatter.com>
2019-12-09 16:02:31 -0800

    Add Typescript

---
+++ ClientApp/src/components/NavMenu.tsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import {
+  Collapse,
+  Container,
+  NavItem,
+  NavLink,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler
+} from 'reactstrap'
+import { Link } from 'react-router-dom'
+import './NavMenu.css'
+
+interface IProps {}
+
+interface IState {
+  collapsed: boolean
+}
+
+export class NavMenu extends React.Component<IProps, IState> {
+  static displayName = NavMenu.name
+
+  constructor(props: IProps) {
+    super(props)
+
+    this.toggleNavbar = this.toggleNavbar.bind(this)
+    this.state = {
+      collapsed: true
+    }
+  }
+
+  toggleNavbar(): void {
+    this.setState({
+      collapsed: !this.state.collapsed
+    })
+  }
+
+  render(): JSX.Element {
+    return (
+      <header>
+        <Navbar
+          className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3"
+          light
+        >
+          <Container>
+            <NavbarBrand tag={Link} to="/">
+              Exit Survey Admin
+            </NavbarBrand>
+            <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+            <Collapse
+              className="d-sm-inline-flex flex-sm-row-reverse"
+              isOpen={!this.state.collapsed}
+              navbar
+            >
+              <ul className="navbar-nav flex-grow">
+                <NavItem>
+                  <NavLink tag={Link} className="text-dark" to="/">
+                    Home
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink tag={Link} className="text-dark" to="/admin-users">
+                    Admin users
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink tag={Link} className="text-dark" to="/employees">
+                    Exiting employees
+                  </NavLink>
+                </NavItem>
+              </ul>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </header>
+    )
+  }
+}
