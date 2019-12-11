a1e5af2b1d4fac378979bb34bd38b0515b2962cd
Frank Hangler <frank@plotandscatter.com>
2019-12-10 19:19:03 -0800

    Very basic log listing

--- ClientApp/src/components/Home.tsx
+++ ClientApp/src/components/Home.tsx
@@ -1,55 +1,63 @@
 import React from 'react'
 
+import {
+  Button,
+  Card,
+  CardBody,
+  CardDeck,
+  CardImg,
+  CardSubtitle,
+  CardText,
+  CardTitle
+} from 'reactstrap'
+
+import './Home.scss'
+
 export class Home extends React.Component {
   render(): JSX.Element {
     return (
       <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li>
-            <a href="https://get.asp.net/">ASP.NET Core</a> and{' '}
-            <a href="https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx">
-              C#
-            </a>{' '}
-            for cross-platform server-side code
-          </li>
-          <li>
-            <a href="https://facebook.github.io/react/">React</a> for
-            client-side code
-          </li>
-          <li>
-            <a href="http://getbootstrap.com/">Bootstrap</a> for layout and
-            styling
-          </li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li>
-            <strong>Client-side navigation</strong>. For example, click{' '}
-            <em>Counter</em> then <em>Back</em> to return here.
-          </li>
-          <li>
-            <strong>Development server integration</strong>. In development
-            mode, the development server from <code>create-react-app</code> runs
-            in the background automatically, so your client-side resources are
-            dynamically built on demand and the page refreshes when you modify
-            any file.
-          </li>
-          <li>
-            <strong>Efficient production builds</strong>. In production mode,
-            development-time features are disabled, and your{' '}
-            <code>dotnet publish</code> configuration produces minified,
-            efficiently bundled JavaScript files.
-          </li>
-        </ul>
-        <p>
-          The <code>ClientApp</code> subdirectory is a standard React
-          application based on the <code>create-react-app</code> template. If
-          you open a command prompt in that directory, you can run{' '}
-          <code>npm</code> commands such as <code>npm test</code> or{' '}
-          <code>npm install</code>.
-        </p>
+        <h1 className="mt-5">Exit Survey Admin web interface</h1>
+        <div className="row">
+          <div className="col-4">
+            <h2>Task logs</h2>
+            <table className="table">
+              <thead>
+                <th></th>
+                <th>Date</th>
+                <th>Status</th>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className="text-success">&#x2B24;</td>
+                  <td>2019/12/10 Tue </td>
+                  <td>Success</td>
+                </tr>
+                <tr>
+                  <td className="text-success">&#x2B24;</td>
+                  <td>2019/12/09 Mon </td>
+                  <td>Success</td>
+                </tr>
+                <tr>
+                  <td className="text-success">&#x2B24;</td>
+                  <td>2019/12/08 Sun </td>
+                  <td>Success</td>
+                </tr>
+                <tr>
+                  <td className="text-success">&#x2B24;</td>
+                  <td>2019/12/07 Sat </td>
+                  <td>Success</td>
+                </tr>
+                <tr>
+                  <td className="text-success">&#x2B24;</td>
+                  <td>2019/12/06 Fri </td>
+                  <td>Success</td>
+                </tr>
+              </tbody>
+            </table>
+            <button className="btn btn-primary">View more</button>
+          </div>
+        </div>
       </div>
     )
   }
