ae2915d59f88e3c7f3695bc6b8754716e5bbaf7b
Frank Hangler <frank@plotandscatter.com>
2020-03-24 10:34:02 -0700

    Auto-format C# files

--- Models/AdminUser.cs
+++ Models/AdminUser.cs
@@ -3,24 +3,24 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ExitSurveyAdmin.Models
 {
-  public class AdminUser : BaseEntity
-  {
-    [Key]
-    public string Id { get; set; }
+    public class AdminUser : BaseEntity
+    {
+        [Key]
+        public string Id { get; set; }
 
-    [Required]
-    public string EmployeeId { get; set; }
+        [Required]
+        public string EmployeeId { get; set; }
 
-    [Required]
-    public string FirstName { get; set; }
+        [Required]
+        public string FirstName { get; set; }
 
-    [Required]
-    public string LastName { get; set; }
+        [Required]
+        public string LastName { get; set; }
 
-    [Required]
-    public string Email { get; set; }
+        [Required]
+        public string Email { get; set; }
 
-    [DataType(DataType.DateTime)]
-    public DateTime LastLoginTs { get; set; }
-  }
+        [DataType(DataType.DateTime)]
+        public DateTime LastLoginTs { get; set; }
+    }
 }
--- Models/Employee.cs
+++ Models/Employee.cs
@@ -3,22 +3,22 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ExitSurveyAdmin.Models
 {
-  public class Employee : BaseEntity
-  {
-    [Required]
-    public string Id { get; set; }
+    public class Employee : BaseEntity
+    {
+        [Required]
+        public string Id { get; set; }
 
-    [StringLength(50, MinimumLength = 1)]
-    [Required]
-    public string FirstName { get; set; }
+        [StringLength(50, MinimumLength = 1)]
+        [Required]
+        public string FirstName { get; set; }
 
-    [StringLength(50, MinimumLength = 1)]
-    [Required]
-    public string LastName { get; set; }
+        [StringLength(50, MinimumLength = 1)]
+        [Required]
+        public string LastName { get; set; }
 
 
-    [DataType(DataType.Date)]
-    [Required]
-    public DateTime BirthDate { get; set; }
-  }
+        [DataType(DataType.Date)]
+        [Required]
+        public DateTime BirthDate { get; set; }
+    }
 }
