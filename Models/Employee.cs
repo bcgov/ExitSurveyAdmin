42007c65e3a57e18b4bb58193571caadfe8a734f
Frank Hangler <frank@plotandscatter.com>
2020-03-24 12:57:41 -0700

    Add all draft Employee fields. Fixes #26.

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -8,17 +8,108 @@ namespace ExitSurveyAdmin.Models
         [Required]
         public string Id { get; set; }
 
-        [StringLength(50, MinimumLength = 1)]
+        [Required]
+        public string EmployeeId { get; set; }
+
         [Required]
         public string FirstName { get; set; }
 
-        [StringLength(50, MinimumLength = 1)]
         [Required]
         public string LastName { get; set; }
-
 
         [DataType(DataType.Date)]
         [Required]
         public DateTime BirthDate { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        public string GovernmentEmail { get; set; }
+
+        [Required]
+        public string Classification { get; set; }
+
+        [Required]
+        public string Ministry { get; set; }
+
+        [Required]
+        public string DepartmentId { get; set; }
+
+        [Required]
+        public string JobFunctionCode { get; set; }
+
+        [Required]
+        public string LocationCity { get; set; }
+
+        [DataType(DataType.Date)]
+        [Required]
+        public DateTime OriginalHireDate { get; set; }
+
+        [DataType(DataType.Date)]
+        [Required]
+        public DateTime LastDayWorkedDate { get; set; }
+
+        [DataType(DataType.Date)]
+        [Required]
+        public DateTime EffectiveDate { get; set; }
+
+        [Required]
+        public string Reason { get; set; }
+
+        [Required]
+        public string Address1 { get; set; }
+
+        [Required]
+        public string Address2 { get; set; }
+
+        [Required]
+        public string AddressCity { get; set; }
+
+        [Required]
+        public string AddressProvince { get; set; }
+
+        [Required]
+        public string AddressPostCode { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+
+        [Required]
+        public string AppointmentStatus { get; set; }
+
+        [Required]
+        public string PositionCode { get; set; }
+
+        [Required]
+        public string Age { get; set; }
+
+        [DataType(DataType.Date)]
+        [Required]
+        public DateTime LeaveDate { get; set; }
+
+        [Required]
+        public string ServiceYears { get; set; }
+
+        [Required]
+        public string JobCode { get; set; }
+
+        [Required]
+        public string BackDated { get; set; }
+
+        [Required]
+        public string ExitCount { get; set; }
+
+        [Required]
+        public string AgeGroup { get; set; }
+
+        [Required]
+        public string ClassificationGroup { get; set; }
+
+        [Required]
+        public string ServiceGroup { get; set; }
+
+        [Required]
+        public string LocationGroup { get; set; }
     }
 }
