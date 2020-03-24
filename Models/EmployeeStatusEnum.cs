03ba7bb6bafbf914d44c2cd68080e5bffb8c86da
Frank Hangler <frank@plotandscatter.com>
2020-03-24 13:55:59 -0700

    Add EmployeeStatusEnum. Fixes #27.

---
+++ Models/EmployeeStatusEnum.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExitSurveyAdmin.Models
+{
+
+
+
+    public class EmployeeStatusEnum
+    {
+
+        [Key]
+        [Required]
+        public string Code { get; set; }
+
+        [Required]
+        public string State { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public List<Employee> Employees { get; set; }
+    }
+}
