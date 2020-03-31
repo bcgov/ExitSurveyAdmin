9d5e0530cc2269900305580393c75f39750adb93
Frank Hangler <frank@plotandscatter.com>
2020-03-31 12:03:39 -0700

    Remove JSON cyclicity

--- Models/TaskEnum.cs
+++ Models/TaskEnum.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace ExitSurveyAdmin.Models
 {
@@ -13,6 +14,7 @@ namespace ExitSurveyAdmin.Models
         [Required]
         public string Description { get; set; }
 
+        [JsonIgnore]
         public List<TaskLogEntry> TaskLogEntries { get; set; }
     }
 }
