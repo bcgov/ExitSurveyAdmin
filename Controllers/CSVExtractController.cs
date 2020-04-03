d5a6fd7f43fd4820fa44c7a4c6cdfa67c3cf056a
Frank Hangler <frank@plotandscatter.com>
2020-04-03 13:22:06 -0700

    Basic API method to read CSVs and return Employees

--- Controllers/CSVExtractController.cs
+++ Controllers/CSVExtractController.cs
@@ -1,3 +1,6 @@
+using System.Globalization;
+using System.Text;
+using System.IO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,6 +9,9 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ExitSurveyAdmin.Models;
+using System.Net.Http;
+using Microsoft.VisualBasic.FileIO;
+using CsvHelper;
 
 namespace ExitSurveyAdmin.Controllers
 ***REMOVED***
@@ -29,6 +35,35 @@ namespace ExitSurveyAdmin.Controllers
                 .ReadAllTextAsync("./SampleInput/PSA-CSV-Sample.csv");
 
             return Content(text);
+      ***REMOVED***
+
+        // GetCSV: Given the raw text of the PSA CSV extract (as obtained, for
+        // instance, from the GetCSV method), transform it into an array of
+        // nicely-formatted Employee JSON objects. Note that these Employees are
+        // NOT saved or otherwise processed by default.
+        // POST: api/CSVExtract/EmployeesFromCSV
+        [HttpPost("EmployeesFromCSV")]
+        public async Task<ActionResult<List<Employee>>> EmployeesFromCSV()
+        ***REMOVED***
+            // By default the content will not be read if it is not form or JSON
+            // type so we need to use a stream reader to read the request body.
+            // CsvReader expects a StreamReader anyways so we will use that.
+            using (StreamReader reader = new StreamReader(Request.Body, Encoding.UTF8))
+            using (CsvReader csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            ***REMOVED***
+                // Use the ClassMap to map the headers in the CSV to the fields
+                // of the Employee model.
+                csv.Configuration.RegisterClassMap<PSACSVMap>();
+
+                var employeeList = new List<Employee>();
+
+                await foreach (Employee e in csv.GetRecordsAsync<Employee>())
+                ***REMOVED***
+                    employeeList.Add(e);
+              ***REMOVED***
+
+                return employeeList;
+          ***REMOVED***
       ***REMOVED***
   ***REMOVED***
 ***REMOVED***
---
+++ Models/PSACSVMap.cs
@@ -0,0 +1,10 @@
+using CsvHelper.Configuration;
+using ExitSurveyAdmin.Models;
+
+public class PSACSVMap : ClassMap<Employee>
+***REMOVED***
+    public PSACSVMap()
+    ***REMOVED***
+        Map(m => m.EmployeeId).Name("EmplID");
+  ***REMOVED***
+***REMOVED***
