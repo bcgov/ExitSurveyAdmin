8cb99b101f0f3cbefeb195a1f5d91e7a75644317
Frank Hangler <frank@plotandscatter.com>
2020-04-06 15:49:50 -0700

    Enumerate values

--- Models/TaskOutcomeEnum.cs
+++ Models/TaskOutcomeEnum.cs
@@ -6,6 +6,25 @@ namespace ExitSurveyAdmin.Models
 {
     public class TaskOutcomeEnum
     {
+        private static readonly string CodeSuccess = "Success";
+        private static readonly string CodeWarn = "Warn";
+        private static readonly string CodeFail = "Fail";
+
+        public static readonly TaskEnum Success = new TaskEnum
+        {
+            Code = CodeSuccess,
+            Description = "The task was successful, with no warnings."
+        };
+        public static readonly TaskEnum Warn = new TaskEnum
+        {
+            Code = CodeWarn,
+            Description = "The task was successful, but there were some warning messages."
+        };
+        public static readonly TaskEnum Fail = new TaskEnum
+        {
+            Code = CodeFail,
+            Description = "The task failed before completing entirely. It may have been partially successful, but must be re-run to ensure a valid outcome."
+        };
 
         [Key]
         [Required]
