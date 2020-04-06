cf993af6b03f3e6dfcb2cf74e875e2a3862d4b25
Frank Hangler <frank@plotandscatter.com>
2020-04-06 15:34:29 -0700

    Enumerate the EmployeeActions

--- Models/EmployeeActionEnum.cs
+++ Models/EmployeeActionEnum.cs
@@ -6,6 +6,49 @@ namespace ExitSurveyAdmin.Models
 ***REMOVED***
     public class EmployeeActionEnum
     ***REMOVED***
+        public static readonly string CODE_CREATE_FROM_CSV = "CreateFromCSV";
+        public static readonly string CODE_CREATE_FROM_DATA_TRANSFER = "CreateFromDataTransfer";
+        public static readonly string CODE_UPDATE_BY_ADMIN = "UpdateByAdmin";
+        public static readonly string CODE_UPDATE_BY_TASK = "UpdateByTask";
+        public static readonly string CODE_STATUS_CHANGE = "StatusChange";
+        public static readonly string CODE_EMAIL_SENT = "EmailSent";
+        public static readonly string CODE_SURVEY_RETRIEVED = "SurveyRetrieved";
+
+        public static readonly EmployeeActionEnum CREATE_FROM_CSV = new EmployeeActionEnum
+        ***REMOVED***
+            Code = CODE_CREATE_FROM_CSV,
+            Description = "The employee has been created by the CSV import task."
+      ***REMOVED***;
+        public static readonly EmployeeActionEnum CREATE_FROM_DATA_TRANSFER = new EmployeeActionEnum
+        ***REMOVED***
+            Code = CODE_CREATE_FROM_DATA_TRANSFER,
+            Description = "The employee has been created via transfer from the old system."
+      ***REMOVED***;
+        public static readonly EmployeeActionEnum UPDATE_BY_ADMIN = new EmployeeActionEnum
+        ***REMOVED***
+            Code = CODE_UPDATE_BY_ADMIN,
+            Description = "The employee has had one or more fields updated by an admin user."
+      ***REMOVED***;
+        public static readonly EmployeeActionEnum UPDATE_BY_TASK = new EmployeeActionEnum
+        ***REMOVED***
+            Code = CODE_UPDATE_BY_TASK,
+            Description = "The employee has had one or more fields updated by the CSV import task."
+      ***REMOVED***;
+        public static readonly EmployeeActionEnum STATUS_CHANGE = new EmployeeActionEnum
+        ***REMOVED***
+            Code = CODE_STATUS_CHANGE,
+            Description = "The employee has had their status changed (see StatusEnumeration)."
+      ***REMOVED***;
+        public static readonly EmployeeActionEnum EMAIL_SENT = new EmployeeActionEnum
+        ***REMOVED***
+            Code = CODE_EMAIL_SENT,
+            Description = "An email was sent. This will normally be immediately followed by a StatusChange entry."
+      ***REMOVED***;
+        public static readonly EmployeeActionEnum SURVEY_RETRIEVED = new EmployeeActionEnum
+        ***REMOVED***
+            Code = CODE_SURVEY_RETRIEVED,
+            Description = "The survey value was retrieved. This will normally be immediately followed by a StatusChange entry."
+      ***REMOVED***;
 
         [Key]
         [Required]
