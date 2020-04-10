558eeeee2242dff6ed4530829e6d61c56b3f8f3f
Frank Hangler <frank@plotandscatter.com>
2020-04-10 14:07:00 -0700

    Add basic callback handler

---
+++ ClientApp/src/components/Login/CallbackHandler.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+interface IProps ***REMOVED******REMOVED***
+
+export default class CallbackHandler extends React.Component<IProps> ***REMOVED***
+  public render(): JSX.Element ***REMOVED***
+    console.log('Arrived here')
+    return <div className="CallbackHandler"></div>
+***REMOVED***
+***REMOVED***
