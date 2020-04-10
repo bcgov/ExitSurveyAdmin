b8d2705b78e982ae779f8ccea5058cd551ee5103
Frank Hangler <frank@plotandscatter.com>
2020-04-10 14:07:26 -0700

    Add persistor

--- ClientApp/src/index.tsx
+++ ClientApp/src/index.tsx
@@ -2,6 +2,11 @@ import 'bootstrap/dist/css/bootstrap.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import ***REMOVED*** BrowserRouter ***REMOVED*** from 'react-router-dom'
+import ***REMOVED*** Provider ***REMOVED*** from 'react-redux'
+import ***REMOVED*** createStore ***REMOVED*** from 'redux'
+import ***REMOVED*** persistReducer, persistStore ***REMOVED*** from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import ***REMOVED*** PersistGate ***REMOVED*** from 'redux-persist/integration/react'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
@@ -10,10 +15,45 @@ const baseUrl = document
   .getAttribute('href') as string
 const rootElement = document.getElementById('root')
 
+const initialState = ***REMOVED***
+  token: null
+***REMOVED***
+
+const persistConfig = ***REMOVED***
+  key: 'ExitSurveyAdmin',
+  storage
+***REMOVED***
+
+const reducer = (state = initialState, action: any): any => ***REMOVED***
+  switch (action.type) ***REMOVED***
+    case 'AUTH_SUCCESS': ***REMOVED***
+      const ***REMOVED*** token ***REMOVED*** = action
+      console.log('AUTH_SUCCESS token', token)
+      return Object.assign(***REMOVED******REMOVED***, state, ***REMOVED***
+        token
+    ***REMOVED***)
+  ***REMOVED***
+    case 'LOGOUT':
+      return Object.assign(***REMOVED******REMOVED***, initialState)
+    default:
+      return state
+***REMOVED***
+***REMOVED***
+
+const persistedReducer = persistReducer(persistConfig, reducer)
+
+export const store = createStore(persistedReducer)
+
+const persistor = persistStore(store)
+
 ReactDOM.render(
-  <BrowserRouter basename=***REMOVED***baseUrl***REMOVED***>
-    <App />
-  </BrowserRouter>,
+  <Provider store=***REMOVED***store***REMOVED***>
+    <PersistGate loading=***REMOVED***'Loading...'***REMOVED*** persistor=***REMOVED***persistor***REMOVED***>
+      <BrowserRouter basename=***REMOVED***baseUrl***REMOVED***>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>,
   rootElement
 )
 
