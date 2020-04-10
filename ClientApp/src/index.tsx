36f04d21351b045b7a9dd75f6dde83d76eb9753c
Frank Hangler <frank@plotandscatter.com>
2020-04-10 14:07:26 -0700

    Add persistor

--- ClientApp/src/index.tsx
+++ ClientApp/src/index.tsx
@@ -2,6 +2,11 @@ import 'bootstrap/dist/css/bootstrap.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import { persistReducer, persistStore } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { PersistGate } from 'redux-persist/integration/react'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
@@ -10,10 +15,45 @@ const baseUrl = document
   .getAttribute('href') as string
 const rootElement = document.getElementById('root')
 
+const initialState = {
+  token: null
+}
+
+const persistConfig = {
+  key: 'ExitSurveyAdmin',
+  storage
+}
+
+const reducer = (state = initialState, action: any): any => {
+  switch (action.type) {
+    case 'AUTH_SUCCESS': {
+      const { token } = action
+      console.log('AUTH_SUCCESS token', token)
+      return Object.assign({}, state, {
+        token
+      })
+    }
+    case 'LOGOUT':
+      return Object.assign({}, initialState)
+    default:
+      return state
+  }
+}
+
+const persistedReducer = persistReducer(persistConfig, reducer)
+
+export const store = createStore(persistedReducer)
+
+const persistor = persistStore(store)
+
 ReactDOM.render(
-  <BrowserRouter basename={baseUrl}>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <PersistGate loading={'Loading...'} persistor={persistor}>
+      <BrowserRouter basename={baseUrl}>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>,
   rootElement
 )
 
