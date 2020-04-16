39896dab48df5138d8e96be6d295fe514de4f9b6
Frank Hangler <frank@plotandscatter.com>
2020-04-16 15:27:15 -0700

    Redirect properly

--- ClientApp/src/components/Login/CallbackHandler.tsx
+++ ClientApp/src/components/Login/CallbackHandler.tsx
@@ -4,12 +4,13 @@ import ***REMOVED*** connect ***REMOVED*** from 'react-redux'
 import ***REMOVED*** CallbackComponent ***REMOVED*** from 'redux-oidc'
 import ***REMOVED*** push ***REMOVED*** from 'connected-react-router'
 import userManager from '../../store/utils/userManager'
+import ***REMOVED*** RouteComponentProps, withRouter ***REMOVED*** from 'react-router-dom'
 
 interface IDispatchProps ***REMOVED***
   dispatch: Dispatch<AnyAction>
 ***REMOVED***
 
-interface IProps extends IDispatchProps ***REMOVED******REMOVED***
+interface IProps extends IDispatchProps, RouteComponentProps ***REMOVED******REMOVED***
 
 class CallbackPage extends React.Component<IProps> ***REMOVED***
   async componentDidMount(): Promise<void> ***REMOVED***
@@ -25,18 +26,17 @@ class CallbackPage extends React.Component<IProps> ***REMOVED***
         userManager=***REMOVED***userManager***REMOVED***
         successCallback=***REMOVED***(): void => ***REMOVED***
           console.log('There was a login success')
-          this.props.dispatch(push('/'))
+          this.props.history.push('/')
       ***REMOVED******REMOVED***
         errorCallback=***REMOVED***(error: any): void => ***REMOVED***
           console.log('There was an error')
-          this.props.dispatch(push('/'))
           console.error(error)
       ***REMOVED******REMOVED***
       >
-        <div>Redirecting...</div>
+        <div>Redirecting... if not, there was an error (see console)</div>
       </CallbackComponent>
     )
 ***REMOVED***
 ***REMOVED***
 
-export default connect()(CallbackPage)
+export default withRouter(connect()(CallbackPage))
