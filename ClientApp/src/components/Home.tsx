7ab96d8882cddc93bb91aab5a74035fe83d79a18
Frank Hangler <frank@plotandscatter.com>
2020-04-17 14:54:15 -0700

    Tidying up code

--- ClientApp/src/components/Home.tsx
+++ ClientApp/src/components/Home.tsx
@@ -6,7 +6,7 @@ class Home extends React.Component {
   render(): JSX.Element {
     return (
       <div className="Home">
-        <h1 className="mt-5">Exit Survey Admin web interface</h1>
+        <h1>Exit Survey Admin web interface</h1>
       </div>
     )
   }
