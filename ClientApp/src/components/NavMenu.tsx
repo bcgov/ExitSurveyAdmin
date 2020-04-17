5eac117047960bb70a665485fbfca22c0c46de68
Frank Hangler <frank@plotandscatter.com>
2020-04-17 13:17:41 -0700

    Remove user name from nav

--- ClientApp/src/components/NavMenu.tsx
+++ ClientApp/src/components/NavMenu.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import ***REMOVED*** Link ***REMOVED*** from 'react-router-dom'
 import './NavMenu.css'
 
-import userManager from '../store/utils/userManager'
-
 interface IProps ***REMOVED******REMOVED***
 
 interface IState ***REMOVED***
@@ -15,19 +13,9 @@ class NavMenu extends React.Component<IProps, IState> ***REMOVED***
 
   constructor(props: IProps) ***REMOVED***
     super(props)
-
-    this.state = ***REMOVED***
-      name: ''
-  ***REMOVED***
-***REMOVED***
-
-  async componentDidMount(): Promise<void> ***REMOVED***
-    const user = await userManager.getUser()
-    this.setState(***REMOVED*** name: user?.profile.given_name ***REMOVED***)
 ***REMOVED***
 
   render(): JSX.Element ***REMOVED***
-    const name = this.state.name
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light mb-3 border-bottom">
         <Link to="/" className="navbar-brand">
@@ -47,7 +35,7 @@ class NavMenu extends React.Component<IProps, IState> ***REMOVED***
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-              <small>Logged in as ***REMOVED***name***REMOVED***</small>
+              ***REMOVED***/* <small>Logged in as ***REMOVED***name***REMOVED***</small> */***REMOVED***
             </li>
           </ul>
           <ul className="navbar-nav ml-auto">
