7e1a1a17c0f8ab503a3354ef9f190cdd4b7865d6
Frank Hangler <frank@plotandscatter.com>
2020-04-17 15:14:42 -0700

    Better unauthorized user catch

--- ClientApp/src/components/Login/Unauthorized.tsx
+++ ClientApp/src/components/Login/Unauthorized.tsx
@@ -1,56 +1,32 @@
 import React from 'react'
-import { connect } from 'react-redux'
 import userManager from '../../store/utils/userManager'
 
 interface IOwnProps {}
 
-interface IStateProps {
-  user: any
-}
+interface IStateProps {}
 
 interface IDispatchProps {}
 
 interface IProps extends IOwnProps, IStateProps, IDispatchProps {}
 
 class Unauthorized extends React.Component<IProps> {
-  constructor(props: IProps) {
-    super(props)
-    this.checkUser = this.checkUser.bind(this)
+  loginClick(): void {
+    userManager.signinRedirect()
   }
 
-  checkUser(): void {
-    const { user } = this.props
-    console.log('--> user', user)
-    if (!user || user.expired) {
-      userManager.signinRedirect()
-    }
-  }
-
-  componentDidMount(): void {
-    console.log('componentDidMount')
-    this.checkUser()
-  }
-
-  componentDidUpdate(): void {
-    console.log('componentDidUpdate')
-    this.checkUser()
-  }
   render(): JSX.Element {
     return (
       <div>
-        <h1>Unauthorized.</h1>
-        <p>Redirecting you&hellip;</p>
+        <h1>You must log in to view this page.</h1>
+        <button
+          className="btn btn-lg btn-outline-primary mt-3"
+          onClick={this.loginClick}
+        >
+          Click here to log in &rarr;
+        </button>
       </div>
     )
   }
 }
 
-const mapStateToProps = (state: any): IStateProps => {
-  if (state && state.oidc && state.oidc.user) {
-    return { user: state.oidc.user }
-  } else {
-    return { user: undefined }
-  }
-}
-
-export default connect(mapStateToProps)(Unauthorized)
+export default Unauthorized
