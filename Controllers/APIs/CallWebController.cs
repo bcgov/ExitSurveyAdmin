af8072891794b3a7df26d12615bc8954458613b5
Frank Hangler <frank@plotandscatter.com>
2020-04-24 14:16:55 -0700

    Basic CallWeb controller with sample data. Fixes #48

---
+++ Controllers/APIs/CallWebController.cs
@@ -0,0 +1,58 @@
+using System.Globalization;
+using System.Text;
+using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ExitSurveyAdmin.Models;
+using System.Net.Http;
+using Microsoft.VisualBasic.FileIO;
+using CsvHelper;
+using ExitSurveyAdmin.Services;
+
+namespace ExitSurveyAdmin.Controllers
+***REMOVED***
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CallWebController : ControllerBase
+    ***REMOVED***
+        private readonly ExitSurveyAdminContext _context;
+        private readonly AppConfiguration _myConfiguration;
+
+        public CallWebController(ExitSurveyAdminContext context, AppConfiguration myConfiguration)
+        ***REMOVED***
+            _context = context;
+            _myConfiguration = myConfiguration;
+      ***REMOVED***
+
+        // GetCSV: Returns the raw, as-is text of the PSA CSV extract.
+        // GET: api/CallWeb/CSV
+        [HttpGet("CSV")]
+        public async Task<ActionResult<string>> GetCSV()
+        ***REMOVED***
+            string text = await CSVService
+                .ReadCSV(_myConfiguration.SampleCallWebCSVFilePath);
+
+            return Content(text);
+      ***REMOVED***
+
+        // GetCSV: Given the raw text of the PSA CSV extract (as obtained, for
+        // instance, from the GetCSV method), transform it into an array of
+        // nicely-formatted Employee JSON objects. Note that these Employees are
+        // NOT saved or otherwise processed by default.
+        // POST: api/CSVExtract/EmployeesFromCSV
+        [HttpPost("EmployeesFromCSV")]
+        public async Task<ActionResult<List<Employee>>> EmployeesFromCSV()
+        ***REMOVED***
+            // Get a list of candidate Employee objects based on the CSV.
+            var csvEmployeeList = await CSVService
+                .EmployeesFromCSV(Request.Body, Encoding.UTF8);
+
+            return csvEmployeeList;
+      ***REMOVED***
+  ***REMOVED***
+***REMOVED***
