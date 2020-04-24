1523eb30a0add7b8627b503640366a9b8027568a
Frank Hangler <frank@plotandscatter.com>
2020-04-24 14:16:00 -0700

    Load AppConfiguration

---
+++ Controllers/Services/AppConfiguration.cs
@@ -0,0 +1,8 @@
+namespace ExitSurveyAdmin.Services
+{
+    public class AppConfiguration
+    {
+        public string SamplePSACSVFilePath { get; set; }
+        public string SampleCallWebCSVFilePath { get; set; }
+    }
+}
