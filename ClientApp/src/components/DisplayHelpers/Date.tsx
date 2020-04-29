8ffffb11838b33fc030af8a1277b42659ff2b09a
Frank Hangler <frank@plotandscatter.com>
2020-04-29 11:36:09 -0700

    Option to show local timezone

--- ClientApp/src/components/DisplayHelpers/Date.tsx
+++ ClientApp/src/components/DisplayHelpers/Date.tsx
@@ -3,19 +3,22 @@ import React from 'react'
 interface IProps ***REMOVED***
   date?: Date
   showTime?: boolean
+  showLocalTimezone?: boolean
 ***REMOVED***
 
 const LOCALE = 'en-ca'
 
 class LabelledText extends React.Component<IProps> ***REMOVED***
   render(): JSX.Element ***REMOVED***
-    const date = this.props.date
+    const ***REMOVED*** date, showTime, showLocalTimezone ***REMOVED*** = this.props
+    const options: Intl.DateTimeFormatOptions = ***REMOVED******REMOVED***
+    if (!showLocalTimezone) options.timeZone = 'UTC'
     return (
       <div className="Date">
-        ***REMOVED***date && this.props.showTime
-          ? date.toLocaleString(LOCALE, ***REMOVED*** timeZone: 'UTC' ***REMOVED***)
+        ***REMOVED***date && showTime
+          ? date.toLocaleString(LOCALE, options)
           : date
-          ? date.toLocaleDateString(LOCALE, ***REMOVED*** timeZone: 'UTC' ***REMOVED***)
+          ? date.toLocaleDateString(LOCALE, options)
           : ''***REMOVED***
       </div>
     )
