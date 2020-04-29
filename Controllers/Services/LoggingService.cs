54e8468f5dc93910e30adf4eefdf5a379d50592e
Frank Hangler <frank@plotandscatter.com>
2020-04-29 15:44:44 -0700

    Code to log API results

---
+++ Controllers/Services/LoggingService.cs
@@ -0,0 +1,53 @@
+using System.Runtime.CompilerServices;
+using System.IO;
+using System.Threading.Tasks;
+using ExitSurveyAdmin.Models;
+
+namespace ExitSurveyAdmin.Services
+{
+    public class LoggingService
+    {
+        // Read a local file from the project.
+        public async static Task<TaskLogEntry> Log(
+            ExitSurveyAdminContext context,
+            TaskEnum task, TaskOutcomeEnum taskOutcome, string comment
+        )
+        {
+            var entry = new TaskLogEntry()
+            {
+                TaskCode = task.Code,
+                TaskOutcomeCode = taskOutcome.Code,
+                Comment = comment
+            };
+
+            context.TaskLogEntries.Add(entry);
+            await context.SaveChangesAsync();
+
+            return entry;
+        }
+
+        public static Task<TaskLogEntry> LogSuccess(
+            ExitSurveyAdminContext context,
+            TaskEnum task, string comment
+        )
+        {
+            return Log(context, task, TaskOutcomeEnum.Success, comment);
+        }
+
+        public static Task<TaskLogEntry> LogWarning(
+            ExitSurveyAdminContext context,
+            TaskEnum task, string comment
+        )
+        {
+            return Log(context, task, TaskOutcomeEnum.Warn, comment);
+        }
+
+        public static Task<TaskLogEntry> LogFailure(
+            ExitSurveyAdminContext context,
+            TaskEnum task, string comment
+        )
+        {
+            return Log(context, task, TaskOutcomeEnum.Fail, comment);
+        }
+    }
+}
