6e20acc826ddcc3ab26868dc8beedd76a760ffd1
Frank Hangler <frank@plotandscatter.com>
2020-05-05 00:09:32 -0700

    Extract LoadingRow to tfoot

---
+++ ClientApp/src/components/DisplayHelpers/LoadingRow.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+interface IProps {
+  loading: boolean
+  pageIndex: number
+  pageSize: number
+  recordCount: number
+}
+
+const LoadingRow = (props: IProps): JSX.Element => {
+  const { loading, pageSize, pageIndex, recordCount } = props
+  const rangeMin = pageIndex * pageSize + 1
+  const rangeMax = Math.min((pageIndex + 1) * pageSize, recordCount)
+  return (
+    <tr>
+      {loading ? (
+        // Use our custom loading state to show a loading indicator
+        <td colSpan={10000}>Loading...</td>
+      ) : (
+        <td colSpan={10000}>
+          Showing {rangeMin} to {rangeMax} of {recordCount} results
+        </td>
+      )}
+    </tr>
+  )
+}
+
+export default LoadingRow
