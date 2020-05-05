a9a1167f19e1328fc36b0c2b67e884a45f38b2b7
Frank Hangler <frank@plotandscatter.com>
2020-05-05 08:02:01 -0700

    Extract ExportData into its own component

--- ClientApp/src/components/Employees/EmployeeListing.tsx
+++ ClientApp/src/components/Employees/EmployeeListing.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
+
 import { Employee, IEmployeeJSON } from '../../types/Employee'
+import { FixTypeLater } from '../../types/FixTypeLater'
 import { requestJSONWithErrorHandler } from '../../helpers/requestHelpers'
 import EmployeeTable from './EmployeeTable'
-import { FixTypeLater } from '../../types/FixTypeLater'
-import { CSVLink } from 'react-csv'
+import ExportData from '../DisplayHelpers/ExportData'
 
-// Maps the sortBy array produced by the react-table to a string that can be
-// used by the server API, of the kind &sorts=Col1,Col2. A minus sign prefixes
-// a desc sort. If the sortBy array is empty, return the empty string.
+/** Maps the sortBy array produced by the react-table to a string that can be
+used by the server API, of the kind &sorts=Col1,Col2. A minus sign prefixes
+a desc sort. If the sortBy array is empty, return the empty string. */
 const processSorts = (sortBy: FixTypeLater): string => {
   return sortBy.length
     ? `&sorts=${sortBy
@@ -16,6 +17,10 @@ const processSorts = (sortBy: FixTypeLater): string => {
     : ''
 }
 
+/** Maps the filters array produced by the react-table to a string that can be
+used by the server API, of the kind &filters=Col1@=someString. The @=
+operator means 'Col1 contains someString'. For a full list of operators see
+the documentation for Sieve: https://github.com/Biarity/Sieve/#operators */
 const processFilters = (filters: FixTypeLater): string => {
   return filters.length
     ? `&filters=${filters
@@ -30,31 +35,23 @@ const EmployeeListing = (): JSX.Element => {
   const [loading, setLoading] = React.useState<boolean>(false)
   const [pageCount, setPageCount] = React.useState<number>(0)
   const [recordCount, setRecordCount] = React.useState<number>(0)
-  const [sortsQs, setSortsQs] = React.useState<string>('')
-  const [filtersQs, setFiltersQs] = React.useState<string>('')
-  const [downloadedData, setDownloadedData] = React.useState<Employee[]>([])
+  const [sortQuery, setSortQuery] = React.useState<string>('')
+  const [filterQuery, setFilterQuery] = React.useState<string>('')
   const fetchIdRef = React.useRef<number>(0)
-  const csvLinkRef = React.useRef(null)
 
   // Called when the table needs new data
   const fetchData = React.useCallback(({ pageIndex, sortBy, filters }) => {
-    console.log('filters', filters)
-
-    // Give this fetch an ID
+    // Give this fetch an ID and set the loading state
     const fetchId = ++fetchIdRef.current
-
-    // Get the sorts argument for the server
-    const sortsQsTmp = processSorts(sortBy)
-    const filtersQsTmp = processFilters(filters)
-
-    // console.log(filterQs)
-
-    // Set the loading state
     setLoading(true)
+
+    // Get the sort and filter querystrings for the server call
+    setSortQuery(processSorts(sortBy))
+    setFilterQuery(processFilters(filters))
 
     if (fetchId === fetchIdRef.current) {
       requestJSONWithErrorHandler(
-        `api/employees?page=${pageIndex + 1}${sortsQsTmp}${filtersQsTmp}`,
+        `api/employees?page=${pageIndex + 1}${sortQuery}${filterQuery}`,
         'get',
         null,
         'EMPLOYEE_NOT_FOUND',
@@ -66,26 +63,10 @@ const EmployeeListing = (): JSX.Element => {
           setPageCount(pageCount)
           setRecordCount(recordCount)
           setLoading(false)
-          setSortsQs(sortsQsTmp)
-          setFiltersQs(filtersQsTmp)
         }
       )
     }
   }, [])
-
-  const downloadData = (): void => {
-    requestJSONWithErrorHandler(
-      `api/employees?pageSize=${100000}${sortsQs}${filtersQs}`,
-      'get',
-      null,
-      'EMPLOYEE_NOT_FOUND',
-      (responseJSON: IEmployeeJSON[]): void => {
-        console.log(responseJSON)
-        setDownloadedData(Employee.deserializeArray(responseJSON))
-        ;(csvLinkRef.current as FixTypeLater).link.click()
-      }
-    )
-  }
 
   return (
     <>
@@ -96,18 +77,7 @@ const EmployeeListing = (): JSX.Element => {
         controlledPageCount={pageCount}
         recordCount={recordCount}
       />
-      <div>
-        <button className="btn btn-primary mt-2" onClick={downloadData}>
-          Export data
-        </button>
-        <CSVLink
-          data={downloadedData}
-          filename="ExitSurveyAdminData.csv"
-          className="hidden"
-          ref={csvLinkRef}
-          target="_blank"
-        />
-      </div>
+      <ExportData sortQuery={sortQuery} filterQuery={filterQuery} />
     </>
   )
 }
