d4f43a12cdf80afe557ec5229fc58c0a66d767ca
Frank Hangler <frank@plotandscatter.com>
2020-05-05 08:02:01 -0700

    Extract ExportData into its own component

---
+++ ClientApp/src/components/DisplayHelpers/ExportData.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { CSVLink } from 'react-csv'
+
+import { Employee, IEmployeeJSON } from '../../types/Employee'
+import { FixTypeLater } from '../../types/FixTypeLater'
+import { requestJSONWithErrorHandler } from '../../helpers/requestHelpers'
+
+interface IProps {
+  sortQuery: string
+  filterQuery: string
+}
+
+const MAX_PAGE_SIZE = 1000000
+
+const ExportData = (props: IProps): JSX.Element => {
+  const { sortQuery, filterQuery } = props
+
+  const [downloadedData, setDownloadedData] = React.useState<Employee[]>([])
+  const csvLinkRef = React.useRef(null)
+
+  const downloadData = React.useCallback((): void => {
+    requestJSONWithErrorHandler(
+      `api/employees?pageSize=${MAX_PAGE_SIZE}${sortQuery}${filterQuery}`,
+      'get',
+      null,
+      'EMPLOYEE_NOT_FOUND',
+      (responseJSON: IEmployeeJSON[]): void => {
+        setDownloadedData(Employee.deserializeArray(responseJSON))
+
+        // Click the hidden CSVLink
+        ;(csvLinkRef.current as FixTypeLater).link.click()
+      }
+    )
+  }, [])
+
+  return (
+    <div>
+      <button className="btn btn-primary mt-2" onClick={downloadData}>
+        Export data
+      </button>
+      <CSVLink
+        data={downloadedData}
+        filename="ExitSurveyAdminData.csv"
+        className="hidden"
+        ref={csvLinkRef}
+        target="_blank"
+      />
+    </div>
+  )
+}
+
+export default ExportData
