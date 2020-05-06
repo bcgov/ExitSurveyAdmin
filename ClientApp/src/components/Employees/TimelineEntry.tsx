f807b5bacb7835285fb22de9642be41a3f6f2ece
Frank Hangler <frank@plotandscatter.com>
2020-05-06 15:58:59 -0700

    Show employee status and action type on TLEntry

--- ClientApp/src/components/Employees/TimelineEntry.tsx
+++ ClientApp/src/components/Employees/TimelineEntry.tsx
@@ -10,13 +10,22 @@ interface IProps {
 class TimelineEntry extends React.Component<IProps> {
   render(): JSX.Element {
     const { timelineEntry: tl } = this.props
+    console.log(tl)
     return (
       <div className="TimelineEntry">
         <hr />
-        <small className="text-muted">
-          <Date showTime showLocalTimezone date={tl.createdTs} />
-        </small>
-        <small>{tl.comment}</small>
+        <div className="mb-2">
+          <small>
+            <span className="text-muted">
+              <Date showTime showLocalTimezone date={tl.createdTs} />
+            </span>
+            <br />
+            <span className="text-muted">Action</span> {tl.employeeActionCode}
+            &nbsp;•&nbsp;
+            <span className="text-muted">Status</span> {tl.employeeStatusCode}
+          </small>
+        </div>
+        {tl.comment}
       </div>
     )
   }
