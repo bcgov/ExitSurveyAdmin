7df3dd1cb4df867da073eb5f2edb615ea33c855f
Frank Hangler <frank@plotandscatter.com>
2020-05-06 15:59:06 -0700

    Build comment form

---
+++ ClientApp/src/components/Employees/AddComment.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+import { requestJSONWithErrorHandler } from '../../helpers/requestHelpers'
+import { AnyJson } from '../../types/JsonType'
+
+interface IProps {
+  employeeDatabaseId: string
+  employeeStatusCode: string
+  refreshDataCallback: () => void
+}
+
+const AddComment = (props: IProps): JSX.Element => {
+  const { employeeDatabaseId, employeeStatusCode } = props
+
+  const [comment, setComment] = React.useState('')
+
+  const submitComment = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault()
+    requestJSONWithErrorHandler(
+      `api/employeetimelineentries`,
+      'post',
+      {
+        EmployeeId: employeeDatabaseId,
+        EmployeeActionCode: 'UpdateByAdmin',
+        EmployeeStatusCode: employeeStatusCode,
+        Comment: comment
+      },
+      'CANNOT_CREATE_EMPLOYEE_TIMELINE_ENTRY',
+      (responseJSON: AnyJson): void => {
+        console.log(responseJSON)
+        props.refreshDataCallback()
+      }
+    )
+  }
+
+  return (
+    <div>
+      <form onSubmit={submitComment}>
+        <textarea
+          className="form-control form-control-sm"
+          value={comment}
+          onChange={(e): void => setComment(e.target.value)}
+          placeholder="Add a timeline comment..."
+        ></textarea>
+        <input
+          type="submit"
+          value="Add comment"
+          className="btn btn-sm btn-primary mt-2"
+        />
+      </form>
+    </div>
+  )
+}
+
+export default AddComment
