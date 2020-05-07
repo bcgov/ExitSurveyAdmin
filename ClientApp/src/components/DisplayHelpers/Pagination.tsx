58937d9908ef95c691ec5252560cc225fe72b8ba
Frank Hangler <frank@plotandscatter.com>
2020-05-07 13:24:47 -0700

    Tweak pagination display

--- ClientApp/src/components/DisplayHelpers/Pagination.tsx
+++ ClientApp/src/components/DisplayHelpers/Pagination.tsx
@@ -21,53 +21,65 @@ const Pagination = (props: IProps): JSX.Element => {
     pageIndex
   } = props
   return (
-    <div className="pagination">
-      <button
-        className="btn btn-primary mr-1"
-        onClick={(): void => gotoPage(0)}
-        disabled={!canPreviousPage}
-      >
-        {'<<'}
-      </button>{' '}
-      <button
-        className="btn btn-primary mr-1"
-        onClick={(): void => previousPage()}
-        disabled={!canPreviousPage}
-      >
-        {'<'}
-      </button>{' '}
-      <button
-        className="btn btn-primary mr-1"
-        onClick={(): void => nextPage()}
-        disabled={!canNextPage}
-      >
-        {'>'}
-      </button>{' '}
-      <button
-        className="btn btn-primary mr-1"
-        onClick={(): void => gotoPage(pageCount - 1)}
-        disabled={!canNextPage}
-      >
-        {'>>'}
-      </button>{' '}
-      <span>
-        Page{' '}
-        <strong>
-          {pageIndex + 1} of {pageCount}
-        </strong>{' '}
-      </span>
-      <span>
-        | Go to page:{' '}
-        <input
-          type="number"
-          defaultValue={pageIndex + 1}
-          onChange={(e): void => {
-            const page = e.target.value ? Number(e.target.value) - 1 : 0
-            gotoPage(page)
-          }}
-          style={{ width: '100px' }}
-        />
-      </span>{' '}
+    <div className="row align-items-center pagination">
+      <div className="col">
+        <button
+          className="btn btn-primary mr-1"
+          onClick={(): void => gotoPage(0)}
+          disabled={!canPreviousPage}
+        >
+          {'<<'}
+        </button>{' '}
+        <button
+          className="btn btn-primary mr-1"
+          onClick={(): void => previousPage()}
+          disabled={!canPreviousPage}
+        >
+          {'<'}
+        </button>{' '}
+        <button
+          className="btn btn-primary mr-1"
+          onClick={(): void => nextPage()}
+          disabled={!canNextPage}
+        >
+          {'>'}
+        </button>{' '}
+        <button
+          className="btn btn-primary mr-1"
+          onClick={(): void => gotoPage(pageCount - 1)}
+          disabled={!canNextPage}
+        >
+          {'>>'}
+        </button>
+      </div>
+      <div className="col text-center">
+        <span>
+          Page{' '}
+          <strong>
+            {pageIndex + 1} of {pageCount}
+          </strong>{' '}
+        </span>
+      </div>
+      <div className="col text-right">
+        <div className="form-group row mb-0">
+          <label className="col-4 col-form-label" htmlFor="GoToPage">
+            Go to page
+          </label>
+          <div className="col-8">
+            <input
+              id="GoToPage"
+              className="form-control form-control-sm"
+              type="number"
+              defaultValue={pageIndex + 1}
+              onChange={(e): void => {
+                const page = e.target.value ? Number(e.target.value) - 1 : 0
+                gotoPage(page)
+              }}
+              style={{ width: '100px' }}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
