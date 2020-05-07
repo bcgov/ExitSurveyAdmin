551965ec1a3701aca7a67b21628341aed0ca058a
Frank Hangler <frank@plotandscatter.com>
2020-05-07 13:24:47 -0700

    Tweak pagination display

--- ClientApp/src/components/DisplayHelpers/Pagination.tsx
+++ ClientApp/src/components/DisplayHelpers/Pagination.tsx
@@ -21,53 +21,65 @@ const Pagination = (props: IProps): JSX.Element => ***REMOVED***
     pageIndex
 ***REMOVED*** = props
   return (
-    <div className="pagination">
-      <button
-        className="btn btn-primary mr-1"
-        onClick=***REMOVED***(): void => gotoPage(0)***REMOVED***
-        disabled=***REMOVED***!canPreviousPage***REMOVED***
-      >
-        ***REMOVED***'<<'***REMOVED***
-      </button>***REMOVED***' '***REMOVED***
-      <button
-        className="btn btn-primary mr-1"
-        onClick=***REMOVED***(): void => previousPage()***REMOVED***
-        disabled=***REMOVED***!canPreviousPage***REMOVED***
-      >
-        ***REMOVED***'<'***REMOVED***
-      </button>***REMOVED***' '***REMOVED***
-      <button
-        className="btn btn-primary mr-1"
-        onClick=***REMOVED***(): void => nextPage()***REMOVED***
-        disabled=***REMOVED***!canNextPage***REMOVED***
-      >
-        ***REMOVED***'>'***REMOVED***
-      </button>***REMOVED***' '***REMOVED***
-      <button
-        className="btn btn-primary mr-1"
-        onClick=***REMOVED***(): void => gotoPage(pageCount - 1)***REMOVED***
-        disabled=***REMOVED***!canNextPage***REMOVED***
-      >
-        ***REMOVED***'>>'***REMOVED***
-      </button>***REMOVED***' '***REMOVED***
-      <span>
-        Page***REMOVED***' '***REMOVED***
-        <strong>
-          ***REMOVED***pageIndex + 1***REMOVED*** of ***REMOVED***pageCount***REMOVED***
-        </strong>***REMOVED***' '***REMOVED***
-      </span>
-      <span>
-        | Go to page:***REMOVED***' '***REMOVED***
-        <input
-          type="number"
-          defaultValue=***REMOVED***pageIndex + 1***REMOVED***
-          onChange=***REMOVED***(e): void => ***REMOVED***
-            const page = e.target.value ? Number(e.target.value) - 1 : 0
-            gotoPage(page)
-        ***REMOVED******REMOVED***
-          style=***REMOVED******REMOVED*** width: '100px' ***REMOVED******REMOVED***
-        />
-      </span>***REMOVED***' '***REMOVED***
+    <div className="row align-items-center pagination">
+      <div className="col">
+        <button
+          className="btn btn-primary mr-1"
+          onClick=***REMOVED***(): void => gotoPage(0)***REMOVED***
+          disabled=***REMOVED***!canPreviousPage***REMOVED***
+        >
+          ***REMOVED***'<<'***REMOVED***
+        </button>***REMOVED***' '***REMOVED***
+        <button
+          className="btn btn-primary mr-1"
+          onClick=***REMOVED***(): void => previousPage()***REMOVED***
+          disabled=***REMOVED***!canPreviousPage***REMOVED***
+        >
+          ***REMOVED***'<'***REMOVED***
+        </button>***REMOVED***' '***REMOVED***
+        <button
+          className="btn btn-primary mr-1"
+          onClick=***REMOVED***(): void => nextPage()***REMOVED***
+          disabled=***REMOVED***!canNextPage***REMOVED***
+        >
+          ***REMOVED***'>'***REMOVED***
+        </button>***REMOVED***' '***REMOVED***
+        <button
+          className="btn btn-primary mr-1"
+          onClick=***REMOVED***(): void => gotoPage(pageCount - 1)***REMOVED***
+          disabled=***REMOVED***!canNextPage***REMOVED***
+        >
+          ***REMOVED***'>>'***REMOVED***
+        </button>
+      </div>
+      <div className="col text-center">
+        <span>
+          Page***REMOVED***' '***REMOVED***
+          <strong>
+            ***REMOVED***pageIndex + 1***REMOVED*** of ***REMOVED***pageCount***REMOVED***
+          </strong>***REMOVED***' '***REMOVED***
+        </span>
+      </div>
+      <div className="col text-right">
+        <div className="form-group row mb-0">
+          <label className="col-4 col-form-label" htmlFor="GoToPage">
+            Go to page
+          </label>
+          <div className="col-8">
+            <input
+              id="GoToPage"
+              className="form-control form-control-sm"
+              type="number"
+              defaultValue=***REMOVED***pageIndex + 1***REMOVED***
+              onChange=***REMOVED***(e): void => ***REMOVED***
+                const page = e.target.value ? Number(e.target.value) - 1 : 0
+                gotoPage(page)
+            ***REMOVED******REMOVED***
+              style=***REMOVED******REMOVED*** width: '100px' ***REMOVED******REMOVED***
+            />
+          </div>
+        </div>
+      </div>
     </div>
   )
 ***REMOVED***
