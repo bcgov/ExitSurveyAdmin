8b6bd802f3c397adc966eb1b8a29735906a3549f
Frank Hangler <frank@plotandscatter.com>
2020-05-19 16:28:14 -0700

    Fix timezone appearance

--- ClientApp/src/helpers/objectHelper.ts
+++ ClientApp/src/helpers/objectHelper.ts
@@ -1,5 +1,21 @@
 import { NullableString } from '../types/NullableString'
 
+// Dates coming over the wire are UTC, but by default this is not understood
+// when creating dates.From https://stackoverflow.com/a/14006555.
+export function createDateAsUTCFromString(dateString: string): Date {
+  const date = new Date(dateString)
+  return new Date(
+    Date.UTC(
+      date.getFullYear(),
+      date.getMonth(),
+      date.getDate(),
+      date.getHours(),
+      date.getMinutes(),
+      date.getSeconds()
+    )
+  )
+}
+
 export function undefinedIfNull<T>(obj: T | null): T | undefined {
   return obj === null ? undefined : obj
 }
@@ -9,5 +25,5 @@ export function emptyStringIfNull<T>(obj: T | null): T | string {
 }
 
 export function dateOrUndefined(dateString: NullableString): Date | undefined {
-  return dateString ? new Date(dateString) : undefined
+  return dateString ? createDateAsUTCFromString(dateString) : undefined
 }
