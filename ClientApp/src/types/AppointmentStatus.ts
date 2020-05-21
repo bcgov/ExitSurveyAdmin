17e14ac2939d7bb77d26f60477df19d496dd5614
Frank Hangler <frank@plotandscatter.com>
2020-05-21 16:41:40 -0700

    Use types on Employee

--- ClientApp/src/types/AppointmentStatus.ts
+++ ClientApp/src/types/AppointmentStatus.ts
@@ -41,8 +41,9 @@ export class AppointmentStatus {
   }
 
   static statusByKey = (
-    key: AppointmentStatusEnum
+    key?: AppointmentStatusEnum
   ): AppointmentStatus | undefined => {
+    if (!key) return undefined
     return AppointmentStatus.statusDictionary()[key]
   }
 
