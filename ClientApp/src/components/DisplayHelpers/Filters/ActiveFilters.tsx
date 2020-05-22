ca54345d26b32e94932ae02f5c9a3eb4b5e9e4b0
Frank Hangler <frank@plotandscatter.com>
2020-05-21 21:28:33 -0700

    Pass filters through

--- ClientApp/src/components/DisplayHelpers/Filters/ActiveFilters.tsx
+++ ClientApp/src/components/DisplayHelpers/Filters/ActiveFilters.tsx
@@ -3,23 +3,23 @@ import React from 'react'
 import ActiveFilterButton from '../Interface/Buttons/ActiveFilterButton'
 
 import './ActiveFilters.scss'
+import { IFilter } from '../../Employees/EmployeeListing'
 
-interface IProps {}
-
-export default class ActiveFilters extends React.Component<IProps> {
-  public render(): JSX.Element {
-    const filters = [
-      { filter: 'Country', value: 'Canada' },
-      { filter: 'Pub. Year', value: '2007 – 2012' }
-    ].map(f => (
-      <ActiveFilterButton key={f.filter} filter={f.filter} value={f.value} />
-    ))
-
-    return (
-      <div className="ActiveFilters">
-        {/* <h3 className="text-muted mb-0">Active filters</h3> */}
-        <div className="Filters mb-0">{filters}</div>
-      </div>
-    )
-  }
+interface IProps {
+  filters: IFilter[]
 }
+
+const ActiveFilters = (props: IProps): JSX.Element => {
+  const filterButtons = props.filters.map(f => (
+    <ActiveFilterButton key={f.id} filter={f.id} value={f.value} />
+  ))
+
+  return (
+    <div className="ActiveFilters">
+      {/* <h3 className="text-muted mb-0">Active filters</h3> */}
+      <div className="Filters mb-0">{filterButtons}</div>
+    </div>
+  )
+}
+
+export default ActiveFilters
--- ClientApp/src/components/DisplayHelpers/Filters/FilterPanel.tsx
+++ ClientApp/src/components/DisplayHelpers/Filters/FilterPanel.tsx
@@ -11,109 +11,105 @@ interface IProps {
   onChangeCallback: (filters: IFilter[]) => void
 }
 
-interface IState {
-  expanded: boolean
-  filters: IFilter[]
-}
+const FilterPanel = (props: IProps): JSX.Element => {
+  const [expanded, setExpanded] = React.useState(false)
+  const [filters, setFilters] = React.useState<IFilter[]>([])
 
-export default class FilterPanel extends React.Component<IProps, IState> {
-  constructor(props: IProps) {
-    super(props)
+  const addFilter = React.useCallback((filter: IFilter): void => {
+    const filtersClone = [...filters]
+    filtersClone.push(filter)
+    setFilters(filtersClone)
+  }, [])
 
-    this.state = {
-      expanded: false,
-      filters: []
-    }
+  const removeFilter = React.useCallback((filter: IFilter): void => {
+    const filtersClone = [...filters]
+    const index = filtersClone.findIndex(f => f.id === filter.id)
+    if (index > -1) filtersClone.splice(index, 1)
+    setFilters(filtersClone)
+  }, [])
 
-    this.toggleExpanded = this.toggleExpanded.bind(this)
-  }
+  React.useEffect(() => props.onChangeCallback(filters), [filters])
 
-  toggleExpanded(): void {
-    this.setState({ expanded: !this.state.expanded })
-  }
+  const expandedHeight = expanded ? '200px' : '0px'
+  const expandButtonText = expanded ? 'Hide' : 'Expand'
+  const expandButtonIcon = `caret-circle-${expanded ? 'down' : 'right'}`
+  const expandedClass = expanded ? 'Expanded' : ''
 
-  public render(): JSX.Element {
-    const expandedHeight = this.state.expanded ? '200px' : '0px'
-    const expandButtonText = this.state.expanded ? 'Hide' : 'Expand'
-    const expandButtonIcon = this.state.expanded
-      ? 'caret-circle-down'
-      : 'caret-circle-right'
-    const expandedClass = this.state.expanded ? 'Expanded' : ''
-
-    return (
-      <div className="FilterPanel">
-        <div className="FilterPanelHeader row py-3">
-          <div className="col">
-            <div className="d-flex align-items-center">
-              <div className="mr-3">
-                <h2 className="mb-0 text-dark">
-                  <i className="far fa-filter mr-2"></i>Filter employees
-                </h2>
-              </div>
-              <div className="mr-3 text-muted px-2 py-1 d-flex align-items-center bg-light border">
-                <div>
-                  <FAIcon name="eye" type="far" />
-                </div>
-                <div className="ml-2" style={{ lineHeight: '100%' }}>
-                  <small>
-                    Showing
-                    <br />
-                    41 of 118
-                  </small>
-                </div>
-              </div>
+  return (
+    <div className="FilterPanel">
+      <div className="FilterPanelHeader row py-3">
+        <div className="col">
+          <div className="d-flex align-items-center">
+            <div className="mr-3">
+              <h2 className="mb-0 text-dark">
+                <i className="far fa-filter mr-2"></i>Filter employees
+              </h2>
+            </div>
+            <div className="mr-3 text-muted px-2 py-1 d-flex align-items-center bg-light border">
               <div>
-                <ActiveFilters />
-                {/* <p className="text-muted mb-0">
+                <FAIcon name="eye" type="far" />
+              </div>
+              <div className="ml-2" style={{ lineHeight: '100%' }}>
+                <small>
+                  Showing
+                  <br />
+                  41 of 118
+                </small>
+              </div>
+            </div>
+            <div>
+              <ActiveFilters filters={filters} />
+              {/* <p className="text-muted mb-0">
                   <small>
                     Current filters showing <strong>24</strong> of 118 results
                   </small>
                 </p> */}
-              </div>
-              <div className="ml-auto">
-                {this.state.expanded && (
-                  <IconButton
-                    iconType="far"
-                    iconName={expandButtonIcon}
-                    label={`${expandButtonText} filters`}
-                    iconRight
-                    iconMarginClasses="ml-2"
-                    colorType="secondary"
-                    classes="NoOutline NoBackground Faded"
-                    iconClasses="fa-lg"
-                    onClick={this.toggleExpanded}
-                  />
-                )}
-                {!this.state.expanded && (
-                  <IconButton
-                    iconType="far"
-                    iconName={expandButtonIcon}
-                    label={`${expandButtonText} filters`}
-                    iconRight
-                    iconMarginClasses="ml-2"
-                    colorType="secondary"
-                    classes="NoOutline NoBackground Faded"
-                    iconClasses="fa-lg"
-                    onClick={this.toggleExpanded}
-                  />
-                )}
-              </div>
+            </div>
+            <div className="ml-auto">
+              {expanded && (
+                <IconButton
+                  iconType="far"
+                  iconName={expandButtonIcon}
+                  label={`${expandButtonText} filters`}
+                  iconRight
+                  iconMarginClasses="ml-2"
+                  colorType="secondary"
+                  classes="NoOutline NoBackground Faded"
+                  iconClasses="fa-lg"
+                  onClick={() => setExpanded(!expanded)}
+                />
+              )}
+              {!expanded && (
+                <IconButton
+                  iconType="far"
+                  iconName={expandButtonIcon}
+                  label={`${expandButtonText} filters`}
+                  iconRight
+                  iconMarginClasses="ml-2"
+                  colorType="secondary"
+                  classes="NoOutline NoBackground Faded"
+                  iconClasses="fa-lg"
+                  onClick={() => setExpanded(!expanded)}
+                />
+              )}
             </div>
           </div>
         </div>
-        <div
-          className={`FilterPanelBody row ${expandedClass}`}
-          style={{
-            maxHeight: expandedHeight,
-            overflow: 'hidden',
-            boxSizing: 'border-box'
-          }}
-        >
-          <div className="col py-3">
-            <FilterForm />
-          </div>
+      </div>
+      <div
+        className={`FilterPanelBody row ${expandedClass}`}
+        style={{
+          maxHeight: expandedHeight,
+          overflow: 'hidden',
+          boxSizing: 'border-box'
+        }}
+      >
+        <div className="col py-3">
+          <FilterForm />
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
+
+export default FilterPanel
