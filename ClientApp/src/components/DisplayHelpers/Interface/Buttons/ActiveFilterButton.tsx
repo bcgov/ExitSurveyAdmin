13de4ab98e74fc1be3675bdbb726aab2af8ecdf6
Frank Hangler <frank@plotandscatter.com>
2020-05-22 15:04:00 -0700

    Format filters and remove them

--- ClientApp/src/components/DisplayHelpers/Interface/Buttons/ActiveFilterButton.tsx
+++ ClientApp/src/components/DisplayHelpers/Interface/Buttons/ActiveFilterButton.tsx
@@ -1,27 +1,36 @@
 import React from 'react'
 import IconButton from './IconButton'
+import { employeeFieldLabels } from '../../../../types/Employee'
+import { IFilter } from '../../../Employees/EmployeeListing'
 
 interface IProps {
   filter: string
   value: string
+  removeFilter: (filter: IFilter) => void
 }
 
 const ActiveFilterButton = (props: IProps) => {
   const { filter, value } = props
+  const label = employeeFieldLabels[filter]
+
+  const remove = (): void => {
+    props.removeFilter({ id: filter, value })
+  }
 
   return (
     <IconButton
       label={
         <React.Fragment>
-          {filter}: <strong>{value}</strong>
+          {label}: <strong>{value}</strong>
         </React.Fragment>
       }
       iconName="times"
-      marginClasses="mr-1 mb-0"
+      marginClasses="mr-1 my-1 mb-0"
       iconMarginClasses="ml-2"
-      colorType="brand-secondary"
+      colorType="primary"
       iconRight
       size="sm"
+      onClick={remove}
     />
   )
 }
