024285e3083ec7ceff171be096db98a83a7b50ef
Frank Hangler <frank@plotandscatter.com>
2020-05-26 14:11:24 -0700

    Allow run-time env variables. Fix #98, fix #99

--- ClientApp/src/store/utils/userManager.js
+++ ClientApp/src/store/utils/userManager.js
@@ -2,19 +2,18 @@
 
 import { createUserManager } from 'redux-oidc'
 import { WebStorageStateStore } from 'oidc-client'
+import env from '@beam-australia/react-env'
 
 const userManagerConfig = {
-  client_id: 'ExitSurveyAdmin',
-  redirect_uri: process.env.REACT_APP_CALLBACK_LOCATION,
-  response_type: 'code',
-  scope: 'openid profile resourceApi',
-  authority: 'https://sso-dev.pathfinder.gov.bc.ca/auth/realms/ytaqhqia',
-  silent_redirect_uri: `${window.location.protocol}//${
-    window.location.hostname
-  }${window.location.port ? `:${window.location.port}` : ''}/silent_renew.html`,
-  automaticSilentRenew: true,
-  filterProtocolClaims: true,
-  loadUserInfo: true,
+  client_id: env('AUTH_CLIENT_ID'),
+  redirect_uri: env('AUTH_CALLBACK_LOCATION'),
+  response_type: env('AUTH_RESPONSE_TYPE'),
+  scope: env('AUTH_SCOPE'),
+  authority: env('AUTH_URL'),
+  silent_redirect_uri: env('AUTH_SILENT_REDIRECT_URL'),
+  automaticSilentRenew: env('AUTH_AUTO_SILENT_RENEW'),
+  filterProtocolClaims: env('AUTH_FILTER_PROTOCOL_CLAIMS'),
+  loadUserInfo: env('AUTH_LOAD_USER_INFO'),
   userStore: new WebStorageStateStore({ store: window.localStorage })
 }
 
