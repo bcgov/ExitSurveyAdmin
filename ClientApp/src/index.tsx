458e1b80a037d2aa47af1ad0ad39736dde8f970d
Frank Hangler <frank@plotandscatter.com>
2020-06-01 13:52:46 -0700

    Better env management. Fixes #102

--- ClientApp/src/index.tsx
+++ ClientApp/src/index.tsx
@@ -1,30 +1,22 @@
-// import 'bootstrap/dist/css/bootstrap.css'
-// import '@bcgov/bootstrap-theme/dist/scss/bootstrap-theme'
 import './App.scss'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-// import { persistStore } from 'redux-persist'
-// import { PersistGate } from 'redux-persist/integration/react'
 import App from './App'
 import { unregister } from './registerServiceWorker'
 import { OidcProvider } from 'redux-oidc'
 import userManager from './store/utils/userManager'
 import store from './store/store'
+import { routerBase as getRouterBasename } from './helpers/envHelper'
 
-const baseUrl = document
-  .getElementsByTagName('base')[0]
-  .getAttribute('href') as string
 const rootElement = document.getElementById('root')
-
-// const persistor = persistStore(store)
 
 ReactDOM.render(
   <Provider store={store}>
     <OidcProvider store={store} userManager={userManager}>
       {/* <PersistGate loading={'Loading...'} persistor={persistor}> */}
-      <BrowserRouter basename={baseUrl}>
+      <BrowserRouter basename={getRouterBasename()}>
         <App />
       </BrowserRouter>
       {/* </PersistGate> */}
