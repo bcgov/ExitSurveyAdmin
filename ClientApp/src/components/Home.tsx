e4fc8f663926f2fd9d0954bcd42c7c0a03356222
Frank Hangler <frank@plotandscatter.com>
2020-06-03 22:41:20 -0700

    Better UX for login

--- ClientApp/src/components/Home.tsx
+++ ClientApp/src/components/Home.tsx
@@ -1,12 +1,30 @@
 import React from 'react'
 
 import './Home.scss'
+import { Link } from 'react-router-dom'
 
 class Home extends React.Component {
   render(): JSX.Element {
     return (
-      <div className="Home">
-        <h1>Exit Survey Admin web interface</h1>
+      <div className="Centered row">
+        <div className="col-6 offset-3">
+          <h1 className="text-primary display-4 my-5">
+            <i className="fas fa-envelope-open-text mr-4"></i>Exit Survey Admin
+          </h1>
+          <h2>Quick filters</h2>
+          <ul>
+            <li>
+              <Link
+                to={{
+                  pathname: '/employees',
+                  search: '&filters=effectiveDate%3C=2020-06-03'
+                }}
+              >
+                Active employees
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     )
   }
