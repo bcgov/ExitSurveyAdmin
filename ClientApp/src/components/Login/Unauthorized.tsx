33468075c23bceeb618017d4cac79d60cbea75ef
Frank Hangler <frank@plotandscatter.com>
2020-06-03 22:22:43 -0700

    Better redirection

--- ClientApp/src/components/Login/Unauthorized.tsx
+++ ClientApp/src/components/Login/Unauthorized.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import userManager from '../../store/utils/userManager'
 import { getSigninRedirectOptions } from '../../helpers/envHelper'
 
@@ -10,31 +10,31 @@ interface IDispatchProps {}
 
 interface IProps extends IOwnProps, IStateProps, IDispatchProps {}
 
-class Unauthorized extends React.Component<IProps> {
-  constructor(props: IProps) {
-    super(props)
+const Unauthorized = (): JSX.Element => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      userManager.signinRedirect(getSigninRedirectOptions())
+    }, 100)
+    return (): void => clearTimeout(timer)
+  }, [])
 
-    this.loginClick = this.loginClick.bind(this)
-  }
-
-  loginClick(): void {
-    console.log('Unauthorized: loginClick()')
-    userManager.signinRedirect(getSigninRedirectOptions())
-  }
-
-  render(): JSX.Element {
-    return (
-      <div>
+  return (
+    <div className="Unauthorized row align-items-center">
+      <div className="col-6 offset-3">
+        <h1 className="text-primary display-3 mb-5">Exit Survey Admin</h1>
         <h1>You must log in to view this page.</h1>
+        <p>If you are not redirected, click the button below.</p>
         <button
           className="btn btn-lg btn-outline-primary mt-3"
-          onClick={this.loginClick}
+          onClick={(): Promise<void> =>
+            userManager.signinRedirect(getSigninRedirectOptions())
+          }
         >
           Click here to log in &rarr;
         </button>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Unauthorized
--- ClientApp/src/components/Wrappers/AuthenticatedRoute.tsx
+++ ClientApp/src/components/Wrappers/AuthenticatedRoute.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 import AuthWrapper from './AuthWrapper'
+import { FixTypeLater } from '../../types/FixTypeLater'
 
-const AuthenticatedRoute = ({ component: Component, ...rest }: any) => (
+const AuthenticatedRoute = ({
+  component: Component,
+  ...rest
+}: FixTypeLater): JSX.Element => (
   <Route
     {...rest}
-    render={props => (
+    render={(props): JSX.Element => (
       <AuthWrapper>
         <Component {...props} />
       </AuthWrapper>
