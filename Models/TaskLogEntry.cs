f76074d6413690c6f92a1051fcce1a31f516f71d
Frank Hangler <frank@plotandscatter.com>
2020-06-10 09:26:34 -0700

    Enable Sieve on TaskLogEntries

--- Models/TaskLogEntry.cs
+++ Models/TaskLogEntry.cs
@@ -1,3 +1,4 @@
+using Sieve.Attributes;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -9,14 +10,17 @@ namespace ExitSurveyAdmin.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
+        [Sieve(CanFilter = true, CanSort = true)]
         [Required]
         public string TaskCode { get; set; }
         public virtual TaskEnum Task { get; set; }
 
+        [Sieve(CanFilter = true, CanSort = true)]
         [Required]
         public string TaskOutcomeCode { get; set; }
         public virtual TaskOutcomeEnum TaskOutcome { get; set; }
 
+        [Sieve(CanFilter = true, CanSort = true)]
         [Required]
         public string Comment { get; set; }
     }
