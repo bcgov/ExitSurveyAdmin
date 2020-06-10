5837c3ee61d70ad8d7cdd543880372bf9896febe
Frank Hangler <frank@plotandscatter.com>
2020-06-10 09:28:14 -0700

    Pagination and sorting for TaskLogEntry. Related to #106

--- ClientApp/src/components/TaskLogEntries/TaskLogEntryListing.tsx
+++ ClientApp/src/components/TaskLogEntries/TaskLogEntryListing.tsx
@@ -1,99 +1,105 @@
 import React from 'react'
-import { TaskLogEntry } from '../../types/TaskLogEntry'
-import Date from '../DisplayHelpers/FormattedDate'
-import { requestJSONWithErrorHandler } from '../../helpers/requestHelpers'
-import TaskOutcome from '../DisplayHelpers/TaskOutcome'
+
 import { FixTypeLater } from '../../types/FixTypeLater'
+import { requestJSONWithErrorHandler } from '../../helpers/requestHelpers'
+import TaskLogEntryTable from './TaskLogEntryTable'
+import ExportData from '../DisplayHelpers/ExportData'
+import { RouteComponentProps, withRouter } from 'react-router'
+// import FilterPanel from '../DisplayHelpers/Filters/FilterPanel'
+// import { MasterFilterHandler } from '../DisplayHelpers/Filters/MasterFilterHandler'
 import { plainToClass } from 'class-transformer'
+import { ITableSort } from '../../types/ReactTable'
+import { TaskLogEntry } from '../../types/TaskLogEntry'
 
-interface IOwnProps {}
-
-interface IStateProps {}
-
-interface IDispatchProps {}
-
-interface IProps extends IOwnProps, IStateProps, IDispatchProps {}
-
-interface IState {
-  taskLogEntries?: TaskLogEntry[]
+/** Maps the sortBy array produced by the react-table to a string that can be
+used by the server API, of the kind &sorts=Col1,Col2. A minus sign prefixes
+a desc sort. If the sortBy array is empty, return the empty string. */
+const processSorts = (sortBy: ITableSort[]): string => {
+  return sortBy.length
+    ? `&sorts=${sortBy
+        .map((s: FixTypeLater) => `${s.desc ? '-' : ''}${s.id}`)
+        .join(',')}`
+    : ''
 }
 
-class TaskLogEntryListing extends React.Component<IProps, IState> {
-  constructor(props: IProps) {
-    super(props)
-    this.state = { taskLogEntries: undefined }
-  }
+interface IProps extends RouteComponentProps {}
 
-  componentDidMount(): void {
-    this.populateData()
-  }
+const TaskLogEntryListing = (props: IProps): JSX.Element => {
+  // console.log('location -->', props.location)
 
-  static renderTaskLogEntriesTable(
-    taskLogEntries: TaskLogEntry[]
-  ): JSX.Element {
-    taskLogEntries.reverse()
-    return (
-      <table className="table table-striped" aria-labelledby="tabelLabel">
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Task</th>
-            <th>Status</th>
-            <th>Comment</th>
-          </tr>
-        </thead>
-        <tbody>
-          {taskLogEntries.map(taskLogEntry => (
-            <tr key={taskLogEntry.id}>
-              <td>
-                <Date
-                  date={taskLogEntry.createdTs}
-                  showLocalTimezone
-                  showTime
-                />
-              </td>
-              <td>{taskLogEntry.taskCode}</td>
-              <td>
-                <TaskOutcome task={taskLogEntry} />
-              </td>
-              <td>{taskLogEntry.comment}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    )
-  }
+  // Set up the table with no data to start
+  const [data, setData] = React.useState<TaskLogEntry[]>([])
+  const [loading, setLoading] = React.useState<boolean>(false)
+  const [pageCount, setPageCount] = React.useState<number>(0)
+  const [recordCount, setRecordCount] = React.useState<number>(0)
+  const [sortQuery, setSortQuery] = React.useState<string>('')
+  const [filterQuery, setFilterQuery] = React.useState<string>('')
+  const fetchIdRef = React.useRef<number>(0)
 
-  render(): JSX.Element {
-    const contents =
-      this.state.taskLogEntries === undefined ? (
-        <p>
-          <em>Loading...</em>
-        </p>
-      ) : (
-        TaskLogEntryListing.renderTaskLogEntriesTable(this.state.taskLogEntries)
-      )
+  // Called when the table needs new data
+  const fetchData = React.useCallback(
+    ({ pageIndex, sortBy }) => {
+      // Give this fetch an ID and set the loading state
+      const fetchId = ++fetchIdRef.current
+      setLoading(true)
 
-    return (
-      <div>
-        <h1 id="tabelLabel">Task log</h1>
-        {contents}
-      </div>
-    )
-  }
+      // Get the sort and filter querystrings for the server call
+      setSortQuery(processSorts(sortBy))
+      // setFilterQuery(processFilters(filters))
 
-  async populateData(): Promise<void> {
-    await requestJSONWithErrorHandler(
-      `api/taskLogEntries`,
-      'get',
-      null,
-      'TASK_LOG_ENTRIES_NOT_FOUND',
-      (responseJSON: FixTypeLater[]): void =>
-        this.setState({
-          taskLogEntries: responseJSON.map(t => plainToClass(TaskLogEntry, t))
-        })
-    )
-  }
+      // console.log(sortBy, sortQuery)
+      // console.log(filters, filterQuery)
+
+      const path = `taskLogEntries?page=${pageIndex +
+        1}${sortQuery}${filterQuery}`
+
+      if (fetchId === fetchIdRef.current) {
+        requestJSONWithErrorHandler(
+          `api/${path}`,
+          'get',
+          null,
+          'TASK_LOG_ENTRY_NOT_FOUND',
+          (responseJSON: FixTypeLater[], pagination: FixTypeLater): void => {
+            const pageCount = pagination.PageCount
+            const recordCount = pagination.RecordCount
+
+            setData(responseJSON.map(t => plainToClass(TaskLogEntry, t)))
+            setPageCount(pageCount)
+            setRecordCount(recordCount)
+            setLoading(false)
+          }
+        )
+      }
+    },
+    [sortQuery, filterQuery]
+  )
+
+  return (
+    <>
+      <h1>Task log entries</h1>
+      <TaskLogEntryTable
+        data={data}
+        fetchData={fetchData}
+        loading={loading}
+        controlledPageCount={pageCount}
+        recordCount={recordCount}
+      />
+      <ExportData
+        sortQuery={sortQuery}
+        filterQuery={filterQuery}
+        apiModelName="taskLogEntries"
+        setDownloadedDataCallback={(
+          responseJSON: FixTypeLater[]
+        ): FixTypeLater[] =>
+          responseJSON.map(t => {
+            delete t.task
+            delete t.taskOutcome
+            return t
+          })
+        }
+      />
+    </>
+  )
 }
 
-export default TaskLogEntryListing
+export default withRouter(TaskLogEntryListing)
