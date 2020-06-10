ea68d38ab6e21b982aa9d9dc3238afa8df6affdc
Frank Hangler <frank@plotandscatter.com>
2020-06-10 08:29:40 -0700

    Use decorators on TaskLogEntry

--- ClientApp/src/types/TaskLogEntry.ts
+++ ClientApp/src/types/TaskLogEntry.ts
@@ -1,45 +1,15 @@
-import IJSONSerializable from '../helpers/IJSONSerializable'
-import { NullableString } from './NullableString'
-import { dateOrUndefined, undefinedIfNull } from '../helpers/objectHelper'
+import { dateOrUndefined } from '../helpers/objectHelper'
+import { Transform } from 'class-transformer'
 
-export interface ITaskLogEntryJSON {
-  id: NullableString
-  taskCode: NullableString
-  taskOutcomeCode: NullableString
-  comment: NullableString
-  createdTs: NullableString
-  modifiedTs: NullableString
-}
-
-export class TaskLogEntry
-  implements IJSONSerializable<TaskLogEntry, ITaskLogEntryJSON> {
+export class TaskLogEntry {
   id?: string
   taskCode?: string
   taskOutcomeCode?: string
   comment?: string
+
+  @Transform((date: string) => dateOrUndefined(date))
   createdTs?: Date
+
+  @Transform((date: string) => dateOrUndefined(date))
   modifiedTs?: Date
-
-  constructor(input: ITaskLogEntryJSON) {
-    this.deserialize(input)
-  }
-
-  deserialize(input: ITaskLogEntryJSON): TaskLogEntry {
-    this.id = undefinedIfNull(input.id)
-    this.taskCode = undefinedIfNull(input.taskCode)
-    this.taskOutcomeCode = undefinedIfNull(input.taskOutcomeCode)
-    this.comment = undefinedIfNull(input.comment)
-    this.createdTs = dateOrUndefined(input.createdTs)
-    this.modifiedTs = dateOrUndefined(input.modifiedTs)
-
-    return this
-  }
-
-  static deserializeArray(input: ITaskLogEntryJSON[] | null): TaskLogEntry[] {
-    if (!input) {
-      return []
-    } else {
-      return input.map(json => new TaskLogEntry(json))
-    }
-  }
 }
