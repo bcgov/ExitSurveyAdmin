27821ff0569313483d00bdaf53186d9bf34b5166
Frank Hangler <frank@plotandscatter.com>
2020-06-15 14:27:07 -0700

    Update CallWeb row field names

--- Controllers/Services/CallWeb/Models/CallWebRowDto.cs
+++ Controllers/Services/CallWeb/Models/CallWebRowDto.cs
@@ -3,11 +3,11 @@ namespace ExitSurveyAdmin.Services.CallWeb
     public partial class CallWebRowDto
     {
         public string Telkey { get; set; }
-        public string Email { get; set; }
+        public string PreferredEmail { get; set; }
         public string Ministry { get; set; }
         public string PositionTitle { get; set; }
         public string AppointmentStatus { get; set; }
-        public string FirstName { get; set; }
+        public string PreferredFirstName { get; set; }
         public string LastName { get; set; }
         public string LeaveReason { get; set; }
         public string LeaveCode { get; set; }
