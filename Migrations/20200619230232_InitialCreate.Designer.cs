70214b0d6053ad57cd8b7a63bbeaeba90cdd34d0
Frank Hangler <frank@plotandscatter.com>
2020-06-19 16:10:18 -0700

    Fix naming error

--- Migrations/20200619230232_InitialCreate.Designer.cs
+++ Migrations/20200619230232_InitialCreate.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ExitSurveyAdmin.Migrations
 {
     [DbContext(typeof(ExitSurveyAdminContext))]
-    [Migration("20200619224534_InitialCreate")]
+    [Migration("20200619230232_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
