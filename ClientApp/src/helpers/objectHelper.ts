10c47eaf178b24876f6b44e80c31bed963f675c7
Frank Hangler <frank@plotandscatter.com>
2020-06-22 13:47:04 -0700

    Show success message when adding comment

--- ClientApp/src/helpers/objectHelper.ts
+++ ClientApp/src/helpers/objectHelper.ts
@@ -1,3 +1,5 @@
+/* globals Promise */
+
 export function undefinedIfNull<T>(obj: T | null): T | undefined {
   return obj === null ? undefined : obj
 }
@@ -8,4 +10,8 @@ export function nullIfUndefined<T>(obj: T | undefined): T | null {
 
 export function emptyStringIfNull<T>(obj: T | null): T | string {
   return obj === null ? '' : obj
+}
+
+export const timeout = (ms: number): Promise<void> => {
+  return new Promise(resolve => setTimeout(resolve, ms))
 }
