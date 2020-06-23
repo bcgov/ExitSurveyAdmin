e7c0741339fda5315dc6d5b6c83ee434473e5030
Frank Hangler <frank@plotandscatter.com>
2020-06-23 12:24:23 -0700

    Serialize dates better. Fix #110

--- ClientApp/src/components/Employees/EmployeeListing.tsx
+++ ClientApp/src/components/Employees/EmployeeListing.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ***REMOVED*** plainToClass ***REMOVED*** from 'class-transformer'
+import moment from 'moment'
 
 import ***REMOVED*** Employee ***REMOVED*** from '../../types/Employee'
 import ***REMOVED*** employeeFilters ***REMOVED*** from '../Filters/Presets/FieldSets/employeeFilters'
@@ -7,6 +8,7 @@ import ***REMOVED*** employeeTableColumns ***REMOVED*** from './employeeTableCol
 import ***REMOVED*** FixTypeLater ***REMOVED*** from '../../types/FixTypeLater'
 import EmployeePresets from '../Filters/Presets/EmployeePresets'
 import GenericListing from '../Listings/GenericListing'
+import ***REMOVED*** defaultDateFormat ***REMOVED*** from '../../helpers/dateHelper'
 
 const EmployeeListing = (): JSX.Element => (
   <GenericListing
@@ -22,6 +24,15 @@ const EmployeeListing = (): JSX.Element => (
       responseJSON.map(e => ***REMOVED***
         delete e.timelineEntries
         delete e.currentEmployeeStatus
+        e.birthDate = moment(e.birthDate).format(defaultDateFormat)
+        e.originalHireDate = moment(e.originalHireDate).format(
+          defaultDateFormat
+        )
+        e.lastDayWorkedDate = moment(e.lastDayWorkedDate).format(
+          defaultDateFormat
+        )
+        e.effectiveDate = moment(e.effectiveDate).format(defaultDateFormat)
+        e.leaveDate = moment(e.leaveDate).format(defaultDateFormat)
         return e
     ***REMOVED***)
   ***REMOVED***
