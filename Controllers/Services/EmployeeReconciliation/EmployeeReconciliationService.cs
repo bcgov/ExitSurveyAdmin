1e277f1aa72eb771579d69faf162fc9ba95f3a08
Frank Hangler <frank@plotandscatter.com>
2020-08-17 21:59:46 -0700

    Update employee status before field updates. Fix #151

--- Controllers/Services/EmployeeReconciliation/EmployeeReconciliationService.cs
+++ Controllers/Services/EmployeeReconciliation/EmployeeReconciliationService.cs
@@ -178,6 +178,13 @@ namespace ExitSurveyAdmin.Services
             {
                 // Case B. The unique user DOES exist in the database.
 
+                // If the employee is marked as "survey complete," skip them.
+                if (existingEmployee.CurrentEmployeeStatusCode
+                    == EmployeeStatusEnum.SurveyComplete.Code)
+                {
+                    return existingEmployee;
+                }
+
                 // If the employee is marked as "not exiting," update their
                 // status back to "exiting".
                 if (existingEmployee.CurrentEmployeeStatusCode
