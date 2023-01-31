3a60c34190fb2d5e8710cdc55b9cee00734184e8
Frank Hangler <frank@plotandscatter.com>
2023-01-31 14:49:44 -0800

    Update code to bring into line with NJSA. WIP

--- ClientApp/src/helpers/requestHelpers.ts
+++ ClientApp/src/helpers/requestHelpers.ts
@@ -2,10 +2,10 @@ import { setError } from './errorHandlingHelper'
 import store from '../store/store'
 import { FixTypeLater } from '../types/FixTypeLater'
 import { AnyJson } from '../types/JsonType'
-import { deploymentUrl } from './envHelper'
+import { apiUrl } from './envHelper'
 
 export const prefixAPI = (path: string): string => {
-  return `${deploymentUrl()}${path}`
+  return `${apiUrl()}${path}`
 }
 
 const requestWithAuthentication = (
@@ -14,13 +14,33 @@ const requestWithAuthentication = (
   body: AnyJson
 ): Promise<Response> => {
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  const token = store.getState()!.oidc!.user!.access_token
+  const token = store.getState()?.oidc?.user?.access_token
   // console.log(token)
   const fetchObject: RequestInit = {
     method,
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${token}`
+    },
+    credentials: 'same-origin'
+    // mode: 'cors'
+  }
+  if (method !== 'get' && body) {
+    fetchObject.body = JSON.stringify(body)
+  }
+  return window.fetch(prefixAPI(url), fetchObject)
+}
+
+const requestWithoutAuthentication = (
+  url: string,
+  method = 'get',
+  body: AnyJson
+): Promise<Response> => {
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const fetchObject: RequestInit = {
+    method,
+    headers: {
+      'Content-Type': 'application/json'
     },
     credentials: 'same-origin'
   }
@@ -50,6 +70,43 @@ export const requestJSONWithErrorHandler = async (
 
   const paginationHeader = response.headers.get('X-Pagination')
   const pagination = paginationHeader ? JSON.parse(paginationHeader) : {}
+
+  console.log(response, response.headers, 'paginationHeader', paginationHeader)
+  console.log(response.headers.get('X-Pagination'))
+
+  if (response.ok) {
+    if (successCallback) {
+      successCallback(json, pagination)
+    }
+    return json
+  } else {
+    setError(store.dispatch, errorCode)
+    return null
+  }
+}
+
+export const requestJSONWithoutAuth = async (
+  url: string,
+  method = 'get',
+  body: AnyJson,
+  errorCode: string,
+  successCallback: (
+    responseJSON: FixTypeLater,
+    pagination?: FixTypeLater
+  ) => void
+): Promise<AnyJson> => {
+  const response = await requestWithoutAuthentication(url, method, body)
+  let json
+  try {
+    json = await response.json()
+  } catch (e) {
+    console.error('getJSONWithErrorHandler: Error decoding JSON from response.')
+  }
+
+  const paginationHeader = response.headers.get('X-Pagination')
+  const pagination = paginationHeader ? JSON.parse(paginationHeader) : {}
+
+  console.log(response, response.headers, 'paginationHeader', paginationHeader)
 
   if (response.ok) {
     if (successCallback) {
--- Controllers/Services/Authentication/Authentication.cs
+++ Controllers/Services/Authentication/Authentication.cs
@@ -14,71 +14,27 @@ namespace ExitSurveyAdmin.Services
         }
 
         public static void SetJwtBearerOptions(
-            JwtBearerOptions options, string authority
+            JwtBearerOptions options,
+            string authority,
+            string audience,
+            string roleClaimType
         )
         {
             options.Authority = authority;
             options.TokenValidationParameters = new TokenValidationParameters
             {
-                //BC Dev Keycloak
-                ValidAudiences = new string[] {
-                    "ExitSurveyAdmin", "account", "realm-management" // TODO: Check on this
-                },
-                RoleClaimType = "role" // roles in the token for the client.
+                ValidAudiences = new string[] { audience },
+                RoleClaimType = roleClaimType
             };
-            options.RequireHttpsMetadata = false; //for test only!
+            options.RequireHttpsMetadata = true;
             options.SaveToken = true;
 
             options.Validate();
         }
 
-        public static void SetAuthorizationOptions(
-            AuthorizationOptions options, string roleName
-        )
+        public static void SetAuthorizationOptions(AuthorizationOptions options, string roleName)
         {
-            options.AddPolicy("UserRole", policy =>
-                policy.RequireClaim("role", $"[{roleName}]")
-            );
+            options.AddPolicy("UserRole", policy => policy.RequireRole(roleName));
         }
-
-        /*                 
-        options.Events = new JwtBearerEvents()
-        {
-
-            OnAuthenticationFailed = async c => 
-            {
-                c.NoResult();
-                c.Response.StatusCode = 500;
-                c.Response.ContentType = "text/plain";
-                //await c.Response.StartAsyncyes("Startup Authentication failed:" + c.Exception.ToString());
-
-            },
-
-            OnTokenValidated = async ctx =>
-            {
-                var token= ctx.SecurityToken;
-                //var jwt = (ctx.SecurityToken as JwtSecurityToken)?.ToString();
-                // get your JWT token here if you need to decode it e.g on https://jwt.io
-                // And you can re-add role claim if it has different name in token compared to what you want to use in your ClaimIdentity:  
-                // AddRoleClaims(ctx.Principal);
-                // return Task.CompletedTask;
-
-                //// Check if the user has an OID claim(oid = object id = user id)
-                //if (!ctx.Principal.HasClaim(c => c.Type == "http://schemas.microsoft.co..."))
-                //{
-                //    ctx.Fail($"The claim 'oid' is not present in the token.");
-                //}
-
-                // ClaimsPrincipal userPrincipal = ctx.Principal;
-
-                ////cu.CreateUser(userPrincipal, services.BuildServiceProvider());
-
-                //var claims = new List<Claim> {new Claim(ClaimTypes.Role, "Admin")};
-                //var appIdentity = new ClaimsIdentity(claims);
-                //ctx.Principal.AddIdentity(appIdentity);
-                //}
-            }
-        };
-         */
     }
 }
