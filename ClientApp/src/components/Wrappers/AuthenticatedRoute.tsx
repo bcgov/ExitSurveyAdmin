bc9361fefcc22e178eabaa0c514801c9c14c6a0d
Frank Hangler <frank@plotandscatter.com>
2023-02-28 17:24:14 -0800

    Remove unused code

--- ClientApp/src/components/Wrappers/AuthenticatedRoute.tsx
+++ ClientApp/src/components/Wrappers/AuthenticatedRoute.tsx
@@ -1,41 +1,8 @@
 import * as React from 'react'
-import { Route, Redirect, RouteComponentProps } from 'react-router-dom'
-import type { RouteProps } from 'react-router-dom'
-import KeycloakService from '../Login/KeycloakService'
+import { Route } from 'react-router-dom'
+
 import { FixTypeLater } from '../../types/FixTypeLater'
 import AuthWrapper from './AuthWrapper'
-
-// interface PrivateRouteParams extends RouteProps {
-//   component:
-//     | React.ComponentType<RouteComponentProps<any>>
-//     | React.ComponentType<any>
-// }
-
-// const AuthenticatedRoute = ({
-//   component: Component,
-//   ...rest
-// }: PrivateRouteParams) => {
-// const isAuthenticated = KeycloakService.isLoggedIn()
-
-// New and Good and Working
-// return (
-//   <Route
-//     {...rest}
-//     render={(props) =>
-//       isAuthenticated ? (
-//         <Component {...props} />
-//       ) : (
-//         <Redirect
-//           to={{
-//             pathname: '/login',
-//             state: { from: props.location },
-//           }}
-//         />
-//       )
-//     }
-//   />
-// )
-// }
 
 const AuthenticatedRoute = ({
   component: Component,
