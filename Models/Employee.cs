67928a5da02becd1785f3afa2c5702bab41893ad
Frank Hangler <frank@plotandscatter.com>
2023-03-01 13:13:37 -0800

    Set default LocationGroup value

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -233,6 +233,10 @@ namespace ExitSurveyAdmin.Models
         // field. This should only be run when the Employee is created.
         public void InstantiateFields()
         ***REMOVED***
+            if (LocationGroup == null)
+            ***REMOVED***
+                LocationGroup = "";
+          ***REMOVED***
             PreferredFirstName = FirstName;
             PreferredFirstNameFlag = false;
             PreferredEmail = GovernmentEmail;
