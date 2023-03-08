45d400305cfd87e3940e86b68cd8fb4e5a106ffc
Frank Hangler <frank@plotandscatter.com>
2023-03-08 09:08:54 -0800

    Add lastModifiedDate for employee. Fix #193

--- Models/SieveCustomFilterMethods.cs
+++ Models/SieveCustomFilterMethods.cs
@@ -8,7 +8,39 @@ namespace ExitSurveyAdmin.Models
     {
         // Override filtering based on createTs. We need to convert the provided
         // date (which will be in Pacific time) to UTC, and filter correctly.
-        protected IQueryable<T> FilterByDate<T>(IQueryable<T> source, string op, string[] values)
+        protected IQueryable<T> FilterByCreateDate<T>(
+            IQueryable<T> source,
+            string op,
+            string[] values
+        )
+            where T : ExitSurveyAdmin.Models.BaseEntity
+        {
+            TimeZoneInfo pacificZone = TimeZoneInfo.FindSystemTimeZoneById("America/Vancouver");
+
+            var dayStartPacific = TimeZoneInfo.ConvertTimeToUtc(
+                DateTime.Parse(values[0]),
+                pacificZone
+            );
+
+            var dayEndPacific = dayStartPacific + new TimeSpan(23, 59, 59);
+
+            if (op.Equals(">="))
+            {
+                return source.Where(item => item.CreatedTs >= dayStartPacific);
+            }
+            else // i.e. <=
+            {
+                return source.Where(item => item.CreatedTs <= dayEndPacific);
+            }
+        }
+
+        // Override filtering based on modifiedTs. We need to convert the provided
+        // date (which will be in Pacific time) to UTC, and filter correctly.
+        protected IQueryable<T> FilterByModifiedDate<T>(
+            IQueryable<T> source,
+            string op,
+            string[] values
+        )
             where T : ExitSurveyAdmin.Models.BaseEntity
         {
             TimeZoneInfo pacificZone = TimeZoneInfo.FindSystemTimeZoneById("America/Vancouver");
@@ -48,7 +80,16 @@ namespace ExitSurveyAdmin.Models
             string[] values
         )
         {
-            return FilterByDate(source, op, values);
+            return FilterByCreateDate(source, op, values);
+        }
+
+        public IQueryable<Employee> LastModifiedDate(
+            IQueryable<Employee> source,
+            string op,
+            string[] values
+        )
+        {
+            return FilterByModifiedDate(source, op, values);
         }
     }
 }
