da1f1001bcdd83a7806b0be02205b158045fd277
Frank Hangler <frank@plotandscatter.com>
2023-03-08 09:53:56 -0800

    Use moment-timezone

--- ClientApp/src/components/Admin/AdminInterface.tsx
+++ ClientApp/src/components/Admin/AdminInterface.tsx
@@ -10,6 +10,7 @@ import EditableStringField from '../DisplayHelpers/Interface/EditableFields/Edit
 import RefreshStatusButton from './RefreshStatusButton'
 import ScheduledLoadAndUpdateButton from './ScheduledLoadAndUpdateButton'
 import LoadPsaApiButton from './LoadPsaApiButton'
+import AdminInterfaceHelp from './AdminInterfaceHelp'
 
 const AdminInterface = (): JSX.Element => {
   const [adminSettings, setAdminSettings] = React.useState<AdminSetting[]>([])
@@ -30,46 +31,53 @@ const AdminInterface = (): JSX.Element => {
 
   return (
     <div className="Centered AdminInterface row">
-      <div className="col-md-10 offset-md-1 col-lg-8 offset-lg-2 col-xl-6 offset-xl-3">
+      <div className="col-md-12 col-lg-10 offset-lg-1">
         <h1>Admin interface</h1>
         {adminSettings.length > 0 && (
           <div className="row">
-            {adminSettings.map((as) => (
-              <ColumnarLabelledText
-                key={as.id}
-                label={as.displayName!}
-                columnClass="col-12"
-              >
-                <EditableStringField
-                  modelPath={'adminSettings'}
-                  validator={(value: string): boolean => {
-                    return !isNaN(+value) && +value > 0
-                  }}
-                  modelDatabaseId={as.id!}
-                  fieldName={'Value'}
-                  fieldValue={as.value!}
-                  ignoreAdminUserName
-                  refreshDataCallback={(): void => {
-                    requestJSONWithErrorHandler(
-                      `api/adminSettings`,
-                      'get',
-                      null,
-                      'ADMIN_SETTINGS_NOT_FOUND',
-                      (responseJSON: FixTypeLater[]): void => {
-                        setAdminSettings(
-                          responseJSON.map((s) =>
-                            plainToInstance(AdminSetting, s)
-                          )
+            <div className="col-6">
+              <div className="row">
+                {adminSettings.map((as) => (
+                  <ColumnarLabelledText
+                    key={as.id}
+                    label={as.displayName!}
+                    columnClass="col-12"
+                  >
+                    <EditableStringField
+                      modelPath={'adminSettings'}
+                      validator={(value: string): boolean => {
+                        return !isNaN(+value) && +value > 0
+                      }}
+                      modelDatabaseId={as.id!}
+                      fieldName={'Value'}
+                      fieldValue={as.value!}
+                      ignoreAdminUserName
+                      refreshDataCallback={(): void => {
+                        requestJSONWithErrorHandler(
+                          `api/adminSettings`,
+                          'get',
+                          null,
+                          'ADMIN_SETTINGS_NOT_FOUND',
+                          (responseJSON: FixTypeLater[]): void => {
+                            setAdminSettings(
+                              responseJSON.map((s) =>
+                                plainToInstance(AdminSetting, s)
+                              )
+                            )
+                          }
                         )
-                      }
-                    )
-                  }}
-                />
-              </ColumnarLabelledText>
-            ))}
-            <RefreshStatusButton />
-            <ScheduledLoadAndUpdateButton />
-            <LoadPsaApiButton />
+                      }}
+                    />
+                  </ColumnarLabelledText>
+                ))}
+              </div>
+              <RefreshStatusButton />
+              <ScheduledLoadAndUpdateButton />
+              <LoadPsaApiButton />
+            </div>
+            <div className="col-6">
+              <AdminInterfaceHelp />
+            </div>
           </div>
         )}
       </div>
