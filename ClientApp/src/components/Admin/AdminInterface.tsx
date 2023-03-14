0562be1d4b24ab8352fd07adb78a0b4e2fbc4a3c
Frank Hangler <frank@plotandscatter.com>
2023-03-13 21:06:12 -0700

    Remove unused button

--- ClientApp/src/components/Admin/AdminInterface.tsx
+++ ClientApp/src/components/Admin/AdminInterface.tsx
@@ -10,7 +10,6 @@ import ColumnarLabelledText from '../DisplayHelpers/Interface/LabelledItems/Colu
 import EditableStringField from '../DisplayHelpers/Interface/EditableFields/EditableStringField'
 import RefreshStatusButton from './RefreshStatusButton'
 import ScheduledLoadAndUpdateButton from './ScheduledLoadAndUpdateButton'
-import LoadPsaApiButton from './LoadPsaApiButton'
 
 const AdminInterface = (): JSX.Element => {
   const [adminSettings, setAdminSettings] = React.useState<AdminSetting[]>([])
@@ -72,7 +71,6 @@ const AdminInterface = (): JSX.Element => {
                 ))}
                 <RefreshStatusButton />
                 <ScheduledLoadAndUpdateButton />
-                {/* <LoadPsaApiButton /> */}
               </div>
             </div>
             <div className="col-6">
