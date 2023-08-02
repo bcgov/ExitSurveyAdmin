cfab17567d65d4283f523f82234ab9e57544b75f
bvdh98 <bvdhest1998@gmail.com>
2023-08-02 11:14:05 -0700

    updated microsoft dotnet packages to version 7

--- ClientApp/src/helpers/envHelper.ts
+++ ClientApp/src/helpers/envHelper.ts
@@ -24,9 +24,13 @@ export const windowLocation = {
 }
 
 export const keycloakCreationOptions = {
-  url: env('AUTH_URL'),
-  realm: env('AUTH_REALM'),
-  clientId: env('AUTH_CLIENT_ID'),
+    //TODO: fix issue with env variables not being pulled
+  /*url: env('AUTH_URL'),*/
+  /*realm: env('AUTH_REALM'),*/
+    /*clientId: env('AUTH_CLIENT_ID'),*/
+    url: 'https://dev.loginproxy.gov.bc.ca/auth',
+    realm:'standard',
+    clientId: 'exit-survey-admin-4373',
 }
 
 export const keycloakInitOptions: KeycloakInitOptions = {
