38962abb15253e630c53ecebb3a8e0769ae93b1f
Dan Ebenal <Dan.Ebenal@gov.bc.ca>
2025-05-21 14:17:35 -0700

    refactor: update imports to use react-router for consistency and remove unused dependencies

--- ClientApp/src/components/Home.tsx
+++ ClientApp/src/components/Home.tsx
@@ -1,6 +1,5 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import React, { type JSX } from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router'
 
 import { FixTypeLater } from '../types/FixTypeLater'
 import { getActiveEmployeesFilter } from './Filters/Presets/Buttons/SetActiveEmployees'
--- ClientApp/src/components/Login/LoggedInInfo.tsx
+++ ClientApp/src/components/Login/LoggedInInfo.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router'
 import { type JSX } from 'react'
 
 import FAIcon from '../DisplayHelpers/Interface/Icons/FAIcon'
