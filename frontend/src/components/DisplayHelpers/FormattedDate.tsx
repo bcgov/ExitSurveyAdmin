4a19f01baca5672c3aa66039e6dd1f4a70187643
Dan Ebenal <Dan.Ebenal@gov.bc.ca>
2025-07-30 14:27:18 -0700

    Refactor date handling in FormattedDate and dateHelper to ensure UTC parsing and extend dayjs plugins consistently

--- frontend/src/components/DisplayHelpers/FormattedDate.tsx
+++ frontend/src/components/DisplayHelpers/FormattedDate.tsx
@@ -3,14 +3,11 @@ import dayjs from 'dayjs'
 import timezone from 'dayjs/plugin/timezone'
 import relativeTime from 'dayjs/plugin/relativeTime'
 import advancedFormat from 'dayjs/plugin/advancedFormat'
+import utc from 'dayjs/plugin/utc'
 import {
   defaultDateFormat,
   defaultNiceDatetimeFormat,
 } from '../../helpers/dateHelper'
-
-dayjs.extend(timezone)
-dayjs.extend(relativeTime)
-dayjs.extend(advancedFormat)
 
 interface Props {
   date?: Date
@@ -29,10 +26,16 @@ const FormattedDate = ({
   showLocalTimezone,
   showTime,
 }: Props): JSX.Element => {
+  dayjs.extend(utc)
+  dayjs.extend(timezone)
+  dayjs.extend(relativeTime)
+  dayjs.extend(advancedFormat)
+
   let dayjsDate = dayjs(date)
 
   if (showLocalTimezone) {
-    dayjsDate = dayjsDate.tz(TIMEZONE)
+    // Treat the input date as UTC and convert to the target timezone
+    dayjsDate = dayjsDate.tz(TIMEZONE);
   }
 
   let displayDate = ''
